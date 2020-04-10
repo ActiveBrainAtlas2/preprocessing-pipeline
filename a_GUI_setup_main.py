ccde044d351414b119455aa50655eb6bfed5ce89
Joe <checkpppp@gmail.com>
2020-04-10 16:08:56 -0700

    more clean a_GUI_setup_main.py

--- a_GUI_setup_main.py
+++ a_GUI_setup_main.py
@@ -3,27 +3,15 @@ import subprocess
 import argparse
 from time import sleep
 
-sys.path.append("utilities")
-from PyQt5.QtCore import *
-from PyQt5.QtGui import QFont, QIntValidator
 from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QLineEdit, QPushButton, QMessageBox, QProgressBar
 
-from utilities.utilities2015 import execute_command
 from utilities.sqlcontroller import SqlController
 from utilities.a_script_preprocess_setup import preprocess_setup
 from utilities.file_location import FileLocationManager
 
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    description='Setup main page')
-parser.add_argument("stack", type=str, help="stack name")
-args = parser.parse_args()
-stack = args.stack
 
-
-# noinspection PyAttributeOutsideInit
 class init_GUI(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, stack, parent=None):
         super(init_GUI, self).__init__(parent)
 
         # Stack specific info, determined from dropdown menu selection
@@ -187,11 +175,13 @@ class init_GUI(QWidget):
         """
         TIF = self.fileLocationManager.tif
         sections = self.sqlController.get_valid_sections(self.stack)
-        self.b_exit.hide()
         size = len(sections.values()) - 1
+
+        self.b_exit.hide()
         self.progress.setMaximum(size)
         self.progress.show()
         self.b_1.hide()
+
         for index, section in enumerate(sections.values()):
             self.progress.setValue(index)
             source = section['source']
@@ -205,6 +195,7 @@ class init_GUI(QWidget):
                 #                -compress lzw \"" + thumbnail_destination + "\"")
             else:
                 print('File {} does not exist'.format(input_fp))
+
         self.progress.hide()
         self.b_exit.show()
 
@@ -213,15 +204,15 @@ class init_GUI(QWidget):
         # close_main_gui( ex, reopen=True )
 
 
-def main():
-    global app
-    app = QApplication(sys.argv)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='Setup main page')
+    parser.add_argument("stack", type=str, help="stack name")
+    args = parser.parse_args()
+    stack = args.stack
 
-    global ex
-    ex = init_GUI()
+    app = QApplication(sys.argv)
+    ex = init_GUI(stack)
     ex.show()
     sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
