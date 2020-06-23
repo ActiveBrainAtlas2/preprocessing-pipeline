7d5c9af4c8708cdd94e2dc705247f20d8557b6f4
Eddy O'Donnell <eddyod@yahoo.com>
2020-06-23 16:30:01 -0700

    Added tqdm to copy thumbnails
    Former-commit-id: 52505edb5c3da6191eb1cde3113514594b485a2a

--- Litao/copy_thumbnails.py
+++ Litao/copy_thumbnails.py
@@ -5,6 +5,8 @@ The section number, channel and path are read from the database.
 import os, sys
 import argparse
 from shutil import copyfile
+from tqdm import tqdm
+
 
 sys.path.append(os.path.join(os.getcwd(), '../'))
 from utilities.sqlcontroller import SqlController
@@ -20,7 +22,7 @@ def copy_thumbnails_to_dir(animal):
 
     for channel in channels:
         valid_sections = sql_controller.get_raw_sections(animal, channel)
-        for section in valid_sections:
+        for section in tqdm(valid_sections):
             src_file = os.path.join(file_location_manager.thumbnail_prep, section.destination)
             channel_dir = 'CH{}'.format(channel)
             dst_file = os.path.join(file_location_manager.prep, channel_dir,
