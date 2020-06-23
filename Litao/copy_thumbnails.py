ef2819e661741e9aed58a4eaed0c9fa360480f84
Eddy O'Donnell <eddyod@yahoo.com>
2020-06-23 13:02:16 -0700

    Added path append to copy thumbnails

--- Litao/copy_thumbnails.py
+++ Litao/copy_thumbnails.py
@@ -2,10 +2,11 @@
 This file will copy the thumbnails of a stack to their designed channel folder
 The section number, channel and path are read from the database.
 """
-import os
+import os, sys
 import argparse
 from shutil import copyfile
 
+sys.path.append(os.path.join(os.getcwd(), '../'))
 from utilities.sqlcontroller import SqlController
 from utilities.file_location import FileLocationManager
 
