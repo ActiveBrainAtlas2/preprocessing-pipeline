8d892ae07f4d7237d049eed581d8fe807d73655c
Eddy O'Donnell <eddyod@yahoo.com>
2020-07-02 11:25:44 -0700

    Added channel as an arg to copy thumbnails
    Former-commit-id: c3b7504fdca7f73a178cbf5b2f21b9b08464a261

--- Litao/copy_thumbnails.py
+++ Litao/copy_thumbnails.py
@@ -13,31 +13,29 @@ from utilities.sqlcontroller import SqlController
 from utilities.file_location import FileLocationManager
 
 
-def copy_thumbnails_to_dir(animal):
+def copy_thumbnails_to_dir(animal, channel):
     sql_controller = SqlController()
 
     file_location_manager = FileLocationManager(animal)
 
-    channels = [1,2,3]
 
-    for channel in channels:
-        valid_sections = sql_controller.get_raw_sections(animal, channel)
-        for section in tqdm(valid_sections):
-            src_file = os.path.join(file_location_manager.thumbnail_prep, section.destination_file)
-            channel_dir = 'CH{}'.format(channel)
-            dst_file = os.path.join(file_location_manager.prep, channel_dir,
-                                    'thumbnail', str(section.section_number).zfill(3) + '.tif')
+    valid_sections = sql_controller.get_raw_sections(animal, channel)
+    for section in tqdm(valid_sections):
+        src_file = os.path.join(file_location_manager.thumbnail_prep, section.destination_file)
+        channel_dir = 'CH{}'.format(channel)
+        dst_file = os.path.join(file_location_manager.prep, channel_dir,
+                                'thumbnail', str(section.section_number).zfill(3) + '.tif')
 
-            os.makedirs(os.path.dirname(dst_file), exist_ok=True)
-            copyfile(src_file, dst_file)
+        os.makedirs(os.path.dirname(dst_file), exist_ok=True)
+        copyfile(src_file, dst_file)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
     parser.add_argument('--animal', help='Enter the animal', required=True)
-
+    parser.add_argument('--channel', help='Enter channel', required=True)
     args = parser.parse_args()
     animal = args.animal
-
-    copy_thumbnails_to_dir(animal)
+    channel = args.channel
+    copy_thumbnails_to_dir(animal, channel)
 
