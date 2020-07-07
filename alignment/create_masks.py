c9d6fa63d01030813030ef8f132255f8deacb1be
Eddy O'Donnell <eddyod@yahoo.com>
2020-07-07 16:17:10 -0700

    Added an additional method to make masks

--- alignment/create_masks.py
+++ alignment/create_masks.py
@@ -13,8 +13,92 @@ import cv2
 import pandas as pd
 
 sys.path.append(os.path.join(os.getcwd(), '../'))
-from utilities.alignment_utility import get_last_2d, place_image
+from utilities.alignment_utility import get_last_2d, linnorm
 from utilities.file_location import FileLocationManager
+
+
+def find_contour_area(mask):
+    area2 = 0
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    c1 = max(contours, key=cv2.contourArea)
+    area1 = cv2.contourArea(c1)
+    idx = get_index(c1, contours)  # 2
+    contours.pop(idx)
+    if len(contours) > 0:
+        c2 = max(contours, key=cv2.contourArea)
+        area2 = cv2.contourArea(c2)
+    return area1 + area2
+
+
+def get_index(array, list_of_arrays):
+    for j, a in enumerate(list_of_arrays):
+        if np.array_equal(array, a):
+            return j
+    return None
+
+
+def fix_with_fill(img):
+    limit = 250
+    dt = np.uint8
+    no_strip, fe = remove_strip(img)
+    if fe != 0:
+        img[:, fe:] = 0  # mask the strip
+    img = (img / 256).astype(dt)
+    h_src = linnorm(img, limit, dt)
+    med = np.median(h_src)
+    h, im_th = cv2.threshold(h_src, med, limit, cv2.THRESH_BINARY)
+    im_floodfill = im_th.copy()
+    h, w = im_th.shape[:2]
+    mask = np.zeros((h + 2, w + 2), np.uint8)
+    cv2.floodFill(im_floodfill, mask, (0, 0), 255)
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+    im_out = im_th | im_floodfill_inv
+
+    stencil = np.zeros(img.shape).astype('uint8')
+    contours, hierarchy = cv2.findContours(im_out, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    lc = []
+    c1 = max(contours, key=cv2.contourArea)
+    lc.append(c1)
+    area1 = cv2.contourArea(c1)
+
+    area2 = 0
+    idx = get_index(c1, contours)  # 2
+    contours.pop(idx)
+    if len(contours) > 0:
+        c2 = max(contours, key=cv2.contourArea)
+        area2 = cv2.contourArea(c2)
+        area3 = area2 * 0.035
+        if area2 > area3:
+            lc.append(c2)
+    cv2.fillPoly(stencil, lc, 255)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
+    mask = cv2.dilate(stencil, kernel, iterations=2)
+    return mask, area1 + area2
+
+
+def fix_with_blob(img):
+    no_strip, fe = remove_strip(img)
+    min_value, threshold = find_threshold(img)
+    ret, threshed = cv2.threshold(no_strip, threshold, 255, cv2.THRESH_BINARY)
+    threshed = np.uint8(threshed)
+    connectivity = 4
+    output = cv2.connectedComponentsWithStats(threshed, connectivity, cv2.CV_32S)
+    labels = output[1]
+    stats = output[2]
+    # Find the blob that corresponds to the section.
+    if fe != 0:
+        img[:, fe:] = 0  # mask the strip
+    row = find_main_blob(stats, img)
+    blob_label = row[1]['blob_label']
+    # extract the blob
+    blob = np.uint8(labels == blob_label) * 255
+    # Perform morphological closing
+    kernel10 = np.ones((10, 10), np.uint8)
+    mask = cv2.morphologyEx(blob, cv2.MORPH_CLOSE, kernel10, iterations=5)
+    return mask
+
 
 def workershell(cmd):
     """
@@ -112,7 +196,7 @@ def create_mask(animal, resolution):
     else:
 
         files = sorted(os.listdir(INPUT))
-
+        areas = []
 
         for i, file in enumerate(tqdm(files)):
             infile = os.path.join(INPUT, file)
@@ -122,42 +206,16 @@ def create_mask(animal, resolution):
                 print('Could not open', infile)
                 continue
             img = get_last_2d(img)
-            no_strip, fe = remove_strip(img)
+            mask = fix_with_blob(img)
+            area = find_contour_area(mask)
+            areas.append(area)
+            if i > 0 and area < (areas[i - 1] * 0.90):
+                mask, area = fix_with_fill(img)
+            areas.append(area)
 
-            min_value, threshold = find_threshold(img)
-            ###### Threshold it so it becomes binary
-            # threshold = 272
-            ret, threshed = cv2.threshold(no_strip, threshold, 255, cv2.THRESH_BINARY)
-            threshed = np.uint8(threshed)
-            ###### Find connected elements
-            # You need to choose 4 or 8 for connectivity type
-            connectivity = 4
-            output = cv2.connectedComponentsWithStats(threshed, connectivity, cv2.CV_32S)
-            # Get the results
-            # The first cell is the number of labels
-            num_labels = output[0]
-            # The second cell is the label matrix
-            labels = output[1]
-            # The third cell is the stat matrix
-            stats = output[2]
-            # The fourth cell is the centroid matrix
-            centroids = output[3]
-            # Find the blob that corresponds to the section.
-            row = find_main_blob(stats, img)
-            blob_label = row[1]['blob_label']
-            # extract the blob
-            blob = np.uint8(labels == blob_label) * 255
-            # Perform morphological closing
-            kernel10 = np.ones((10, 10), np.uint8)
-            closing = cv2.morphologyEx(blob, cv2.MORPH_CLOSE, kernel10, iterations=5)
-            del blob
-            if fe != 0:
-                img[:,fe:]=0 # mask the strip
-            # scale and mask
-            scaled, _max = scale_and_mask(img, closing)
             # save the mask
             outpath = os.path.join(MASKED, file)
-            cv2.imwrite(outpath, closing.astype('uint8'))
+            cv2.imwrite(outpath, mask.astype('uint8'))
 
             # save the good scaled as CH1
             #outpath = os.path.join(OUTPUT, file)
