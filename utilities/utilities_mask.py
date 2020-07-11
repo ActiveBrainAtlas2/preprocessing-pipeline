b1cbcbe6f28b62ef044177682dbdbfd09a54cf09
Eddy O'Donnell <eddyod@yahoo.com>
2020-07-11 13:35:45 -0700

    Moved methods in clean mask to utilities_mask
    Former-commit-id: b91e374c801827322f5a9f21ed5b2248efa8f26c

--- utilities/utilities_mask.py
+++ utilities/utilities_mask.py
@@ -12,6 +12,28 @@ def get_last_2d(data):
     return data.flat[:m * n].reshape(m, n)
 
 
+def fix_with_blob(img):
+    no_strip, fe = remove_strip(img)
+    min_value, threshold = find_threshold(img)
+    ret, threshed = cv2.threshold(no_strip, threshold, 255, cv2.THRESH_BINARY)
+    threshed = np.uint8(threshed)
+    connectivity = 4
+    output = cv2.connectedComponentsWithStats(threshed, connectivity, cv2.CV_32S)
+    labels = output[1]
+    stats = output[2]
+    # Find the blob that corresponds to the section.
+    if fe != 0:
+        img[:, fe:] = 0  # mask the strip
+    row = find_main_blob(stats, img)
+    blob_label = row[1]['blob_label']
+    # extract the blob
+    blob = np.uint8(labels == blob_label) * 255
+    # Perform morphological closing
+    kernel10 = np.ones((10, 10), np.uint8)
+    mask = cv2.morphologyEx(blob, cv2.MORPH_CLOSE, kernel10, iterations=5)
+    return mask
+
+
 def find_main_blob(stats, image):
     height, width = image.shape
     df = pd.DataFrame(stats)
@@ -40,7 +62,6 @@ def scale_and_mask(src, mask, epsilon=0.01):
     scaled = scaled * (mask > 10)
     return scaled, _max
 
-
 def find_threshold(src):
     fig = matplotlib.figure.Figure()
     ax = matplotlib.axes.Axes(fig, (0, 0, 0, 0))
@@ -48,8 +69,24 @@ def find_threshold(src):
     del ax, fig
     min_point = np.argmin(n[:5])
     min_point = int(min(2, min_point))
-    thresh = (min_point * 64000 / 360) + 300
+    thresh = (min_point * 64000 / 360) + 100
     return min_point, thresh
+
+def remove_strip(src):
+    strip_max = 170;
+    strip_min = 2  # the range of width for the stripe
+    projection=np.sum(src,axis=0)/10000.
+    diff=projection[1:]-projection[:-1]
+    loc,=np.nonzero(diff[-strip_max:-strip_min]>50)
+    mval=np.max(diff[-strip_max:-strip_min])
+    no_strip=np.copy(src)
+    fe = 0
+    if loc.shape[0]>0:
+        loc=np.min(loc)
+        from_end=strip_max-loc
+        fe = -from_end - 2
+        no_strip[:,fe:]=0 # mask the strip
+    return no_strip, fe
 
 
 def make_mask(img):
@@ -92,24 +129,6 @@ def make_mask(img):
     return closing, scaled
 
 
-def remove_strip(src):
-    strip_max = 70;
-    strip_min = 5  # the range of width for the stripe
-
-    projection = np.sum(src, axis=0) / 10000.
-    diff = projection[1:] - projection[:-1]
-    loc, = np.nonzero(diff[-strip_max:-strip_min] > 50)
-    mval = np.max(diff[-strip_max:-strip_min])
-    no_strip = np.copy(src)
-    fe = 0
-    if loc.shape[0] > 0:
-        loc = np.min(loc)
-        from_end = strip_max - loc
-        fe = -from_end - 2
-        no_strip[:, fe:] = 0  # mask the strip
-    return no_strip, fe
-
-
 def place_image(img, max_width, max_height, bgcolor=None):
     zmidr = max_height // 2
     zmidc = max_width // 2
@@ -132,47 +151,6 @@ def place_image(img, max_width, max_height, bgcolor=None):
 def rotate_image(img, rotation):
     img = np.rot90(img, rotation)
     return img
-
-
-def apply_mask(img, mask, stain, rotation, flip):
-    max_width = 1400
-    max_height = 900
-
-    img = get_last_2d(img)
-    dt = img.dtype
-
-    start_bottom = img.shape[0] - 5
-    bottom_rows = img[start_bottom:img.shape[0], :]
-    avg = np.mean(bottom_rows)
-    bgcolor = int(round(avg))
-
-    if dt == np.dtype('uint16'):
-        limit = 2 ** 16 - 1
-        mask16 = np.copy(mask).astype(dt)
-        mask16[mask16 > 0] = limit
-        mask = mask16
-    else:
-        limit = 2 ** 8 - 1
-        limit = bgcolor
-        mask[mask > 0] = limit
-        mask = limit - mask
-        mask = place_image(mask, max_width, max_height, bgcolor)
-
-    if stain == 'NTB':
-        fixed = cv2.bitwise_and(img, mask)
-    else:
-        fixed = cv2.bitwise_or(img, mask)
-
-    if rotation > 0:
-        fixed = rotate_image(fixed, rotation)
-
-    if flip == 'flip':
-        fixed = np.flip(fixed)
-    elif flip == 'flop':
-        fixed = np.flip(fixed, axis=1)
-    fixed = place_image(fixed, max_width, max_height, bgcolor)
-
-    return fixed
 
 
 def pad_with_black(img):
