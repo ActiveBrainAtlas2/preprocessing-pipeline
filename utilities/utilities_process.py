dc9ba2c8db6b9492a8737ab67b9399d17fcb24c0
Eddy O'Donnell <eddyod@yahoo.com>
2020-07-21 11:58:34 -0700

    Added the thionin create to the main dir. Removed shell=True from workershell

--- utilities/utilities_process.py
+++ utilities/utilities_process.py
@@ -2,16 +2,16 @@ import os
 import subprocess
 
 
-def workershell(cmd):
+def workernoshell(cmd):
     """
     Set up an shell command. That is what the shell true is for.
     Args:
-        cmd:  a command line program with arguments in a string
+        cmd:  a command line program with arguments in a list
     Returns: nothing
     """
-    stderr_template = os.path.join(os.getcwd(), 'workershell.err.log')
-    stdout_template = os.path.join(os.getcwd(), 'workershell.log')
+    stderr_template = os.path.join(os.getcwd(), 'workernoshell.err.log')
+    stdout_template = os.path.join(os.getcwd(), 'workernoshell.log')
     stdout_f = open(stdout_template, "w")
     stderr_f = open(stderr_template, "w")
-    proc = subprocess.Popen(cmd, shell=True, stderr=stderr_f, stdout=stdout_f)
+    proc = subprocess.Popen(cmd, shell=False, stderr=stderr_f, stdout=stdout_f)
     proc.wait()
