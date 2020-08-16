e6297964fc047f9a0b3b1dd3bb9d214760f73d51
Eddy O'Donnell <eddyod@yahoo.com>
2020-08-16 08:13:54 -0700

    Added hist equalization to create mask thionin
    Former-commit-id: d3610cbc1d9da9b630c8c60fa9035544c0f82b85

--- neuroglancer/build_volumes.py
+++ neuroglancer/build_volumes.py
@@ -14,9 +14,15 @@ MD589 = np.load(MD589_VOLUME_PATH)
 xy_ng_resolution_um = 5
 
 height, width, zdim = MD589.shape
+height, width, zdim = (800,1200,288)
 
-#full_brain_volume_annotated = np.zeros((zdim,height,width), dtype=np.uint8)
-full_brain_volume_annotated = np.zeros((MD589.shape), dtype=np.uint8)
+full_brain_volume_annotated = np.zeros((zdim,height,width), dtype=np.uint8)
+
+#full_brain_volume_annotated = np.zeros((height, width, zdim), dtype=np.uint8)
+#full_brain_volume_annotated = np.zeros((height, width, zdim), dtype=np.uint8)
+
+
+#full_brain_volume_annotated = np.zeros((height, width, zdim), dtype=np.uint8)
 files = os.listdir(VOL_DIR)
 numpy_files = [f for f in files if f.endswith('.npy') and 'surround' not in f]
 print('full brain volume shape:', full_brain_volume_annotated.shape)
@@ -34,36 +40,35 @@ for n in numpy_files:
         sided = '{}_R'.format(structure.upper())
         color = colors[sided]
 
+    #if structure not in ['SC', 'Sp5C_R']:
+    #    continue
 
-    volume_filename = os.path.join(VOL_DIR, '{}.npy'.format(structure))
+    threshold = 0.90
+    volume_filename = os.path.join(VOL_DIR, f'{structure}.npy')
     volume_input = np.load(volume_filename)
-    volume_input = np.swapaxes(volume_input, 0, 2)
-
-    volume_nonzero_indices = volume_input > 0
+    volume_nonzero_indices = volume_input >= threshold
+    volume_nonone_indices = volume_input < threshold
     volume_input[volume_nonzero_indices] = color
+    volume_input[volume_nonone_indices] = 0
     structure_volume = volume_input.astype(np.uint8)
 
-    origin_filename = os.path.join(VOL_DIR, '{}_origin_wrt_canonicalAtlasSpace.txt'.format(structure))
+    origin_filename = os.path.join(VOL_DIR, f'{structure}_origin_wrt_canonicalAtlasSpace.txt')
     origin_wrt = np.loadtxt(origin_filename)
-    x,y,z = origin_wrt
+    x, y, z = origin_wrt
 
-
-    z_start = 30
-    y_start = int(y) + midy
-    x_start = int(x) + midx
+    z_start = 89
+    factor = 1
+    y_start = int(y*factor) + midy
+    x_start = int(x*factor) + midx
     x_end = x_start + structure_volume.shape[2]
     y_end = y_start + structure_volume.shape[1]
     z_end = z_start + structure_volume.shape[0]
 
     try:
         full_brain_volume_annotated[z_start:z_end, y_start:y_end,x_start:x_end] = structure_volume
-        print('Fit',str(structure).ljust(8), str(color).rjust(2), end="\t")
-        print('shape', str(structure_volume.shape).rjust(18), end=", ")
-        print('x range', str(x_start).rjust(4), str(x_end).rjust(4), end=", ")
-        print('y range', str(y_start).rjust(4), str(y_end).rjust(4), end=", ")
-        print('z range', str(z_start).rjust(4), str(z_end).rjust(4))
+        #full_brain_volume_annotated[y_start:y_end, x_start:x_end, z_start:z_end] = structure_volume
     except:
-        print('Error',str(structure).ljust(8), str(color).rjust(2), end="\t")
+        print('Error', str(structure).ljust(8), str(color).rjust(2), end="\t")
         print('shape', str(structure_volume.shape).rjust(18), end=", ")
         print('x range', str(x_start).rjust(4), str(x_end).rjust(4), end=", ")
         print('y range', str(y_start).rjust(4), str(y_end).rjust(4), end=", ")
