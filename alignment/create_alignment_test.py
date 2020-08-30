f20d5a8736cf2a7c6d0a5c2946a76e4edf74435b
Eddy O'Donnell <eddyod@yahoo.com>
2020-08-30 10:20:00 -0700

    Added more param files

--- alignment/create_alignment_test.py
+++ alignment/create_alignment_test.py
@@ -46,7 +46,6 @@ def parameter_elastix_parameter_file_to_dict(filename):
                     except ValueError:
                         value = v
                 d[key] = value
-
         return d
 
 
@@ -225,6 +224,14 @@ def run_offsets(animal, transforms, jobname, bgcolor):
         op_str += ' -crop {}x{}+0.0+0.0!'.format(max_width, max_height)
         projections = "%(sx)f,%(rx)f,%(ry)f,%(sy)f,%(tx)f,%(ty)f " % {
             'sx': T[0, 0], 'sy': T[1, 1], 'rx': T[1, 0], 'ry': T[0, 1], 'tx': T[0, 2], 'ty': T[1, 2]}
+        #'x': '+' + str(x_tb * scale_factor) if int(x_tb) >= 0 else str(x_tb * scale_factor)
+        #'y': '+' + str(y_tb * scale_factor) if int(y_tb) >= 0 else str(y_tb * scale_factor)
+
+        x = 0
+        crop_x = f"+{x}"
+        if x < 0:
+            crop_x = str(x * SCALING_FACTOR)
+
         crop = '{}x{}+0.0+0.0!'.format(max_width, max_height)
         input_fp = os.path.join(INPUT, file)
         output_fp = os.path.join(OUTPUT, file)
@@ -232,14 +239,15 @@ def run_offsets(animal, transforms, jobname, bgcolor):
         if os.path.exists(output_fp):
             continue
 
-        #cmd = "convert {}  +repage -virtual-pixel background -background \"{}\" {} -flatten -compress lzw {}"\
-        #    .format(input_fp, bgcolor, op_str, output_fp)
-        cmd = ['convert', input_fp, '+repage', '-virtual-pixel',
+        cmd = "convert {}  +repage -virtual-pixel background -background \"{}\" {} -flatten -compress lzw {}"\
+            .format(input_fp, bgcolor, op_str, output_fp)
+        cmd2 = ['convert', input_fp, '+repage', '-virtual-pixel',
                'background', '-background', bgcolor,
                '+distort', 'AffineProjection', projections, '-crop', crop,
                '-flatten', '-compress', 'lzw', output_fp]
         #commands.append(cmd)
-        subprocess.run(cmd)
+        print(cmd)
+        subprocess.run(cmd, shell=True)
 
     #with Pool(njobs) as p:
     #    p.map(workernoshell, commands)
