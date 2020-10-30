9b68c68949ba5f8c40587ed27f014074c5474c32
Eddy O'Donnell <eddyod@yahoo.com>
2020-10-30 09:02:28 -0700

    fixing z start and end in create volume atlas

--- atlas/create_volume_from_atlas.needs.rotating.py
+++ atlas/create_volume_from_atlas.needs.rotating.py
@@ -18,7 +18,7 @@ sys.path.append(PATH)
 from utilities.sqlcontroller import SqlController
 from utilities.file_location import FileLocationManager
 from utilities.utilities_cvat_neuroglancer import get_structure_number, NumpyToNeuroglancer, get_segment_properties
-from utilities.utilities_affine import rigid_transform_3D, ralign, affine_fit
+from utilities.utilities_affine import align_point_sets, DK52_centers, rigid_transform_3D, ralign
 
 
 def create_atlas(animal, create):
@@ -40,7 +40,7 @@ def create_atlas(animal, create):
     sqlController = SqlController(animal)
     resolution = sqlController.scan_run.resolution
     surface_threshold = 0.8
-    SCALE = (10 / 0.46)
+    SCALE = (10 / resolution)
 
     structure_volume_origin = {}
     for volume_filename, origin_filename in zip(volume_files, origin_files):
@@ -67,34 +67,8 @@ def create_atlas(animal, create):
     atlasV7_volume = np.zeros(( int(row_length), int(col_length), z_length), dtype=np.uint8)
     print('atlas volume shape', atlasV7_volume.shape)
 
-    DK52_centers = {'12N': [46488, 18778, 242],
-                    '5N_L': [38990, 20019, 172],
-                    '5N_R': [39184, 19027, 315],
-                    '7N_L': [42425, 23190, 166],
-                    '7N_R': [42286, 22901, 291]}
     ##### actual data for both sets of points, pixel coordinates
-    MD589_centers = {'10N_L': [31002.069009677187, 17139.273764067697, 210],
-                     '10N_R': [30851.821452912456, 17026.27799914138, 242],
-                     '4N_L': [25238.351916435207, 13605.972626040299, 210],
-                     '4N_R': [25231.77274616, 13572.152382002621, 236],
-                     '5N_L': [25863.93885802854, 16448.49802904827, 160],
-                     '5N_R': [25617.920248719453, 16089.048882550318, 298],
-                     '7N_L': [27315.217906796195, 18976.4921239128, 174],
-                     '7N_R': [27227.134448911638, 18547.6538128018, 296],
-                     '7n_L': [26920.538205417844, 16996.292850204114, 177],
-                     '7n_R': [26803.347723222105, 16688.23325135847, 284],
-                     'Amb_L': [29042.974021303286, 18890.218579368557, 167],
-                     'Amb_R': [28901.503217056554, 18291.072163747285, 296],
-                     'DC_L': [28764.5378815116, 15560.1247992853, 134],
-                     'DC_R': [28519.240424058273, 14960.063579837733, 330],
-                     'LC_L': [26993.749068166835, 15146.987356709138, 180],
-                     'LC_R': [26951.610128773387, 14929.363532303963, 268],
-                     'Pn_L': [23019.18002537938, 17948.490571838032, 200],
-                     'Pn_R': [23067.16403704933, 17945.89008778571, 270],
-                     'SC': [24976.373217129738, 10136.880464106176, 220],
-                     'Tz_L': [25210.29041867189, 18857.20817842522, 212],
-                     'Tz_R': [25142.520897455783, 18757.457820947686, 262]}
-    centers = OrderedDict(MD589_centers)
+    centers = OrderedDict(DK52_centers)
     centers_list = []
     for value in centers.values():
         centers_list.append((value[1]/SCALE, value[0]/SCALE, value[2]))
@@ -121,40 +95,40 @@ def create_atlas(animal, create):
     pprint(ATLAS)
     #####Steps
     #trn = Affine_Fit(ATLAS, COM)
-    R, t = rigid_transform_3D(ATLAS.T, COM.T)
+    # source is atlas, output is animal
+    r_auto, t_auto = align_point_sets(ATLAS.T, COM.T)
+    debug = False
 
     for structure, (volume, origin) in sorted(structure_volume_origin.items()):
         print(str(structure).ljust(7),end=": ")
 
         # transformed atlas
-        arr = np.array(atlas_all_centers[structure])
-        #results = trn.Transform(arr)
-        #results = np.dot(R, arr + t)
-        #midrow = results[0]
-        #midcol = results[1]
-        #midz = results[2]
-        input = arr + t
-        results = np.dot(R, input.T)
-        midrow = results[0][0]
-        midcol = results[1][0]
-        midz = results[2][0]
+        source_point = np.array(atlas_all_centers[structure])
+        results = (r_auto @ source_point + t_auto.T).reshape(1,3)
+        y = results[0][0]
+        x = results[0][1]
+        z = results[0][2]
+        y = y - volume.shape[1]/2
+        x = x - volume.shape[0]/2
+        z = z - volume.shape[2]/2
+        #x_start = int( round(x + col_length / 2))
+        #y_start = int( round(y + row_length / 2))
+        #z_start = int(z) // 2 + z_length // 2
+        x_start = int( round(x))
+        y_start = int( round(y))
+        z_start = int(z)
+        x_end = int( round(x_start + volume.shape[0]))
+        y_end = int( round(y_start + volume.shape[1]))
+        z_end = int( round(z_start + (volume.shape[2] + 1) // 2))
 
-        row_start = int(round(midrow - volume.shape[1] / 2))
-        col_start = int(round(midcol - volume.shape[0] / 2))
-        sec_start = int(round(midz - (volume.shape[2] / 2) / 2))
-        row_end = row_start + volume.shape[1]
-        col_end = col_start + volume.shape[0]
-        sec_end = int(round(sec_start + (volume.shape[2] + 1) // 2))
-
-        ## generic atlas
-        x, y, z = origin
-        x_start = int( round(x + col_length / 2))
-        y_start = int( round(y + row_length / 2))
-        z_start = int(z) // 2 + z_length // 2
-        x_end = x_start + volume.shape[0]
-        y_end = y_start + volume.shape[1]
-        z_end = z_start + (volume.shape[2] + 1) // 2
-
+        if debug:
+            print('Midpoints row',
+                  str(int(y*SCALE)).rjust(4),
+                  'mid col',
+                  str(int(x*SCALE)).rjust(4),
+                  'mid z',
+                  str(int(z)).rjust(4),
+                  end=" ")
         print('Row range',
               str(y_start).rjust(4),
               str(y_end).rjust(4),
@@ -166,32 +140,20 @@ def create_atlas(animal, create):
               str(z_end).rjust(4),
               end=" ")
 
-        print('Trans range: row',
-              str(round(row_start,1)).rjust(4),
-              str(round(row_end,1)).rjust(4),
-              'col',
-              str(round(col_start,1)).rjust(4),
-              str(round(col_end,1)).rjust(4),
-              'z',
-              str(round(z_start,1)).rjust(4),
-              str(round(z_end,1)).rjust(4),
-              end=" ")
-
         if structure in centers.keys():
-            xo,yo,zo = MD589_centers[structure]
+            xo,yo,zo = DK52_centers[structure]
             print('COM off by:',
-                  round(midrow*SCALE-yo, 2),
-                  round(midcol*SCALE-xo, 2),
-                  round(midz - zo, 2),
+                  round(x*SCALE - xo, 2),
+                  round(y*SCALE - yo, 2),
+                  round(z - zo, 2),
                   end=" ")
 
         z_indices = [z for z in range(volume.shape[2]) if z % 2 == 0]
         volume = volume[:, :, z_indices]
-        #volume = np.swapaxes(volume, 0, 1)
-        volume = np.rot90(volume, axes=(0,1))
+        volume = np.swapaxes(volume, 0, 1)
         try:
-            atlasV7_volume[row_start:row_end, col_start:col_end, sec_start:sec_end] += volume
-            #atlasV7_volume[y_start:y_end, x_start:x_end, z_start:z_end] += volume
+            atlasV7_volume[y_start:y_end, x_start:x_end, z_start:z_end] += volume
+            #atlasV7_volume[col_start:col_end, row_start:row_end, z_start:z_end] += volume
         except ValueError as ve:
             print(ve, end=" ")
 
@@ -203,6 +165,11 @@ def create_atlas(animal, create):
     print('Resolution at', resolution)
 
     if create:
+        atlasV7_volume = np.rot90(atlasV7_volume, axes=(0, 1))
+        atlasV7_volume = np.fliplr(atlasV7_volume)
+        atlasV7_volume = np.flipud(atlasV7_volume)
+        atlasV7_volume = np.fliplr(atlasV7_volume)
+
         offset = [0,0,0]
         ng = NumpyToNeuroglancer(atlasV7_volume, [resolution, resolution, 20000], offset=offset)
         ng.init_precomputed(OUTPUT_DIR)
@@ -219,7 +186,7 @@ def create_atlas(animal, create):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
-    parser.add_argument('--animal', help='Enter the animal', required=False, default='MD589')
+    parser.add_argument('--animal', help='Enter the animal', required=False, default='DK52')
     parser.add_argument('--create', help='create volume', required=False, default='false')
     args = parser.parse_args()
     animal = args.animal
--- atlas/create_volume_from_atlas.py
+++ atlas/create_volume_from_atlas.py
@@ -15,90 +15,149 @@ start = timer()
 HOME = os.path.expanduser("~")
 PATH = os.path.join(HOME, 'programming/pipeline_utility')
 sys.path.append(PATH)
+from utilities.sqlcontroller import SqlController
 from utilities.file_location import FileLocationManager
 from utilities.utilities_cvat_neuroglancer import get_structure_number, NumpyToNeuroglancer, get_segment_properties
-from utilities.utilities_affine import align_point_sets, load_atlas_data, estimate_structure_centers, umeyama, \
-    rigid_transform_3D
+from utilities.utilities_affine import align_point_sets, DK52_centers, rigid_transform_3D, ralign
 
 
 def create_atlas(animal, create):
+
     fileLocationManager = FileLocationManager(animal)
+    atlas_name = 'atlasV7'
+    DATA_PATH = '/net/birdstore/Active_Atlas_Data/data_root'
+    ROOT_DIR = '/net/birdstore/Active_Atlas_Data/data_root/pipeline_data'
+    THUMBNAIL_DIR = os.path.join(ROOT_DIR, animal, 'preps', 'CH1', 'thumbnail')
+    ATLAS_PATH = os.path.join(DATA_PATH, 'atlas_data', atlas_name)
+    ORIGIN_PATH = os.path.join(ATLAS_PATH, 'origin')
+    VOLUME_PATH = os.path.join(ATLAS_PATH, 'structure')
     OUTPUT_DIR = os.path.join(fileLocationManager.neuroglancer_data, 'atlas')
     if os.path.exists(OUTPUT_DIR):
         shutil.rmtree(OUTPUT_DIR)
     os.makedirs(OUTPUT_DIR, exist_ok=True)
+    origin_files = sorted(os.listdir(ORIGIN_PATH))
+    volume_files = sorted(os.listdir(VOLUME_PATH))
+    sqlController = SqlController(animal)
+    resolution = sqlController.scan_run.resolution
+    surface_threshold = 0.8
+    SCALE = (10 / resolution)
 
-    atlas_data = load_atlas_data()
-    source_centers = estimate_structure_centers(atlas_data, animal)
-    # DK52 structure centers
-    output_centers = {
-        '12N': [46488, 18778, 242],
-        '5N_L': [38990, 20019, 172],
-        '5N_R': [39184, 19027, 315],
-        '7N_L': [42425, 23190, 166],
-        '7N_R': [42286, 22901, 291]
-    }
-    structures = sorted(output_centers.keys())
-    source_point_set = np.array([source_centers[s] for s in structures]).T
-    # Unify to 1 mum scale in all axes
-    source_scale = np.diagflat([10, 10, 20])
-    #source_point_set = source_scale @ source_point_set
-    output_point_set = np.array([output_centers[s] for s in structures]).T
-    pprint(output_centers)
-    pprint(source_centers)
-    # Unify to 1 mum scale in all axes
-    #output_scale = np.diagflat([0.325, 0.325, 0.325])
-    output_scale = np.array([0.325, 0.325, 0.325])
-    #output_point_set = output_scale @ output_point_set
-    r_auto, t_auto = align_point_sets(source_point_set, output_point_set)
-    #t_auto_out = (np.linalg.inv(output_scale) @ t_auto).T
-    #r_auto[2][2] = 1
-    pprint(r_auto)
-    pprint(t_auto)
+    structure_volume_origin = {}
+    for volume_filename, origin_filename in zip(volume_files, origin_files):
+        structure = os.path.splitext(volume_filename)[0]
+        if structure not in origin_filename:
+            print(structure, origin_filename)
+            break
 
-    #for structure, (volume, origin) in sorted(structure_volume_origin.items()):
-    for structure, source_point in sorted(source_centers.items()):
-        print(str(structure).ljust(6), end=": ")
+        color = get_structure_number(structure.replace('_L', '').replace('_R', ''))
+
+        origin = np.loadtxt(os.path.join(ORIGIN_PATH, origin_filename))
+        volume = np.load(os.path.join(VOLUME_PATH, volume_filename))
+
+        volume = np.rot90(volume, axes=(0, 1))
+        volume = np.flip(volume, axis=0)
+        volume[volume > surface_threshold] = color
+        volume = volume.astype(np.uint8)
+
+        structure_volume_origin[structure] = (volume, origin)
+
+    col_length = sqlController.scan_run.width/SCALE
+    row_length = sqlController.scan_run.height/SCALE
+    z_length = len(os.listdir(THUMBNAIL_DIR))
+    atlasV7_volume = np.zeros(( int(row_length), int(col_length), z_length), dtype=np.uint8)
+    print('atlas volume shape', atlasV7_volume.shape)
+
+    ##### actual data for both sets of points, pixel coordinates
+    centers = OrderedDict(DK52_centers)
+    centers_list = []
+    for value in centers.values():
+        centers_list.append((value[1]/SCALE, value[0]/SCALE, value[2]))
+    COM = np.array(centers_list)
+    atlas_com_centers = OrderedDict()
+    atlas_all_centers = {}
+    for structure, (volume, origin) in sorted(structure_volume_origin.items()):
+        midcol, midrow, midz = origin
+        row_start = midrow + row_length / 2
+        col_start = midcol + col_length / 2
+        z_start = midz / 2 + z_length / 2
+        row_end = row_start + volume.shape[0]
+        col_end = col_start + volume.shape[1]
+        z_end = z_start + (volume.shape[2] + 1) / 2
+        midcol = (col_end + col_start) / 2
+        midrow = (row_end + row_start) / 2
+        midz = (z_end + z_start) / 2
+        if structure in centers.keys():
+            atlas_com_centers[structure] = [midrow, midcol, midz]
+        atlas_all_centers[structure] = [midrow, midcol, midz]
+    ATLAS_centers = OrderedDict(atlas_com_centers)
+    ATLAS = np.array(list(ATLAS_centers.values()))
+    pprint(COM)
+    pprint(ATLAS)
+    #####Steps
+    #trn = Affine_Fit(ATLAS, COM)
+    # source is atlas, output is animal
+    r_auto, t_auto = align_point_sets(ATLAS.T, COM.T)
+    debug = True
+
+    for structure, (volume, origin) in sorted(structure_volume_origin.items()):
+        print(str(structure).ljust(7),end=": ")
+
+        # transformed atlas
+        source_point = np.array(atlas_all_centers[structure])
         results = (r_auto @ source_point + t_auto.T).reshape(1,3)
-        print(results*0.325,end="\n")
-        continue
-
-        x, y, z = origin
-        arr = np.array([y, x, z])
-        input = arr + t
-        results = np.dot(r_auto, input.T).reshape(3,1)
         y = results[0][0]
-        x = results[1][0]
-        z = results[2][0]
-        x_start = int(round(x + col_length / 2))
-        y_start = int(round(y + row_length / 2))
-        z_start = int(round(z / 2 + z_length / 2))
-        x_end = x_start + volume.shape[0]
-        y_end = y_start + volume.shape[1]
-        z_end = int(z_start + (volume.shape[2] + 1) / 2)
+        x = results[0][1]
+        z = results[0][2]
+        y = y - volume.shape[1]/2
+        x = x - volume.shape[0]/2
+        x_start = int( round(x))
+        y_start = int( round(y))
+        z_start = int( round(z - volume.shape[2] / 2))
+        x_end = int( round(x_start + volume.shape[0]))
+        y_end = int( round(y_start + volume.shape[1]))
+        z_end = int( round(z_start + (volume.shape[2] + 1) // 2))
 
-        print('Y', str(y_start).rjust(4),  str(y_end).rjust(4),
-              'X',  str(x_start).rjust(4), str(x_end).rjust(4),
-              'Z',  str(z_start).rjust(4), str(z_end).rjust(4),
-              end=" ")
+        if debug:
+            print('volume shape', volume.shape, end=" ")
+            print('Midpoints row',
+                  str(int(y)).rjust(4),
+                  'mid col',
+                  str(int(x)).rjust(4),
+                  'mid z',
+                  str(int(z)).rjust(4),
+                  end=" ")
+            print('Row range',
+                  str(y_start).rjust(4),
+                  str(y_end).rjust(4),
+                  'col range',
+                  str(x_start).rjust(4),
+                  str(x_end).rjust(4),
+                  'z range',
+                  str(z_start).rjust(4),
+                  str(z_end).rjust(4),
+                  end=" ")
 
-        print()
-        continue
+        if structure in centers.keys():
+            xo,yo,zo = DK52_centers[structure]
+            print('COM off by:',
+                  round(x*SCALE - xo, 2),
+                  round(y*SCALE - yo, 2),
+                  round(z - zo, 2),
+                  end=" ")
+
         z_indices = [z for z in range(volume.shape[2]) if z % 2 == 0]
         volume = volume[:, :, z_indices]
         volume = np.swapaxes(volume, 0, 1)
-
         try:
             atlasV7_volume[y_start:y_end, x_start:x_end, z_start:z_end] += volume
         except ValueError as ve:
-            print(ve, end=" ")
+            print('Bad fit', end=" ")
 
         print()
 
+    resolution = int(resolution * 1000 * SCALE)
+    print('Shape of downsampled atlas volume', atlasV7_volume.shape)
 
-    #resolution = int(resolution * 1000 * SCALE)
-    print('Shape of downsampled atlas volume after swapping'.ljust(60), atlasV7_volume.shape)
-    resolution = 10000
     print('Resolution at', resolution)
 
     if create:
@@ -106,9 +165,8 @@ def create_atlas(animal, create):
         atlasV7_volume = np.fliplr(atlasV7_volume)
         atlasV7_volume = np.flipud(atlasV7_volume)
         atlasV7_volume = np.fliplr(atlasV7_volume)
-        print('Shape of downsampled atlas volume after rotating/flipping'.ljust(60), atlasV7_volume.shape)
 
-        offset =  [0,0,0]
+        offset = [0,0,0]
         ng = NumpyToNeuroglancer(atlasV7_volume, [resolution, resolution, 20000], offset=offset)
         ng.init_precomputed(OUTPUT_DIR)
         ng.add_segment_properties(get_segment_properties())
