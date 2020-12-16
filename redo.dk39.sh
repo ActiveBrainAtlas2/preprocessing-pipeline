52b88ef3ae6b077cea7220659dbd26b4d8099204
Eddy O'Donnell <eddyod@yahoo.com>
2020-12-16 10:11:17 -0800

    Put all the steps in redo

--- redo.dk39.sh
+++ redo.dk39.sh
@@ -4,8 +4,16 @@
 rm -f /net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_aligned/*.tif
 rm -f /net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_cleaned/*.tif
 python create_clean.py --animal DK39 --channel 1 --rotation 1 --flip flip
-#cp -vf /net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_cleaned/215.tif \
-#/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_cleaned/216.tif
-#cp -vf /net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_cleaned/218.tif \
-#/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_cleaned/217.tif
+cp -vf /net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_cleaned/217.tif \
+/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_cleaned/216.tif
+cd registration
+python create_registration.py --animal DK39 --iterations 1
+cd ..
+rm -f /net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_aligned/*.tif
+rm -f /net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK39/preps/CH1/thumbnail_cleaned/*.tif
+python create_clean.py --animal DK39 --channel 1 --rotation 1 --flip flip
+cd registration
+python process_registration.py
+cd ..
+echo "finished"
 
