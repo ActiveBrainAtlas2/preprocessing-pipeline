206ff9e206ef1b781a03544a205ce35fccee9f93
Eddy O'Donnell <eddyod@yahoo.com>
2020-12-18 13:30:22 -0800

    added channel in tif2jp2

--- registration/tif2jp2.sh
+++ registration/tif2jp2.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if (($# < 1))
+if (($# < 2))
 then
-    echo "No input brainname argument... exiting"
+    echo "No input brainname,channel argument... exiting"
     exit 1
 fi
 #define input
 ANIMAL=$1
+CHANNEL=$2
 
 
 MATLABCMD="/usr/local/bin/matlab -nodisplay -nodesktop -nosplash -r "
 
 #define path
 INPUT=/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/$ANIMAL/tif/
-OUTPUT=/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/$ANIMAL/jp2/
+OUTPUT=/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/$ANIMAL/jp2/$CHANNEL/
 mkdir -p $OUTPUT
 $MATLABCMD "maxNumCompThreads(2); tif2jp2('$INPUT', '$OUTPUT'); exit"
