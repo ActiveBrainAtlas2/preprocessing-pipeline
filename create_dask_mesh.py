dcbc0c4367b59b01afeffea4eda3aa2cf6bc3a56
Eddy O'Donnell <eddyod@yahoo.com>
2021-01-28 07:04:35 -0800

    Redid create_mesh for memmap

--- create_dask_mesh.py
+++ create_dask_mesh.py
@@ -6,12 +6,16 @@ import os
 import sys
 from concurrent.futures.process import ProcessPoolExecutor
 
+import imageio
 import imagesize
 import numpy as np
 import shutil
 
+from dask_image.imread import imread
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
+import dask
+import dask.array as da
 from tqdm import tqdm
 
 HOME = os.path.expanduser("~")
@@ -21,42 +25,45 @@ from utilities.file_location import FileLocationManager
 from utilities.utilities_cvat_neuroglancer import NumpyToNeuroglancer, get_segment_ids, get_cpus
 
 
-def create_mesh(animal, limit):
+def create_mesh(animal):
     fileLocationManager = FileLocationManager(animal)
-    INPUT = os.path.join(fileLocationManager.prep, 'CH1/downsampled_33')
+    COPY_FROM = os.path.join(fileLocationManager.prep, 'CH1/downsampled_cropped')
+    INPUT = os.path.join(fileLocationManager.prep, 'CH1/small')
+    allfiles = sorted(os.listdir(COPY_FROM))
+    scale = 2
+    """
+    if os.path.exists(INPUT):
+        shutil.rmtree(INPUT)
+    os.makedirs(INPUT, exist_ok=True)
+    for i, f in enumerate(allfiles):
+        if i % scale == 0:
+            source = os.path.join(COPY_FROM, f)
+            dest = os.path.join(INPUT, f)
+            shutil.copy(source, dest)
+    """
     OUTPUT_DIR = os.path.join(fileLocationManager.neuroglancer_data, 'mesh')
     if os.path.exists(OUTPUT_DIR):
         shutil.rmtree(OUTPUT_DIR)
     os.makedirs(OUTPUT_DIR, exist_ok=True)
-    scale = 3
-    allfiles = sorted(os.listdir(INPUT))
-    files = [f for i,f in enumerate(allfiles) if i % scale == 0]
+    files = sorted(os.listdir(INPUT))
     midpoint = len(allfiles) // 2
-    midfilepath = os.path.join(INPUT, allfiles[midpoint])
-    width, height = imagesize.get(midfilepath)
-    if limit > 0:
-        midpoint = len(files) // 2
-        files = files[midpoint-limit:midpoint+limit]
+    midfilepath = os.path.join(COPY_FROM, allfiles[midpoint])
+    midfile = imageio.imread(midfilepath)
+    height, width = midfile.shape
+    print('shape', height, width)
 
-    resolution = 1000 * scale
-    scales = (resolution, resolution, resolution)
+    resolution = 1000
+    scales = (resolution*scale, resolution*scale, resolution*scale)
     chunk_size = [64, 64, 64]
     volume_size = (width, height, len(files))
+    bigdask = imread(f'{INPUT}/*.tif')
     outpath = os.path.join(fileLocationManager.prep, 'bigarray.npy')
-    bigwriter = np.memmap(outpath, dtype=np.uint8, mode="w+", shape=volume_size)
-    for i,f in enumerate(tqdm(files)):
-        infile = os.path.join(INPUT, f)
-        image = Image.open(infile)
-        width, height = image.size
-        array = np.array(image, dtype=np.uint8, order='F')
-        array = array.reshape((height, width,1)).T
-        bigwriter[:, :, i] = array
-        image.close()
-
-    bigreader = np.memmap(outpath, dtype=np.uint8, mode="r+", shape=volume_size)
-    del bigwriter
-    ng = NumpyToNeuroglancer(bigreader, scales, 'segmentation', np.uint8, chunk_size)
+    bigarray = np.memmap(outpath, dtype=np.uint8, mode="w+", shape=(len(files), height, width))
+    bigarray[:] = bigdask
+    bigarray = np.swapaxes(bigarray, 0, 2)
+    ng = NumpyToNeuroglancer(bigarray, scales, 'segmentation', np.uint8, chunk_size)
     ng.init_volume(OUTPUT_DIR)
+    del bigarray
 
     fake_volume = np.zeros(3) + 255
     ng.add_segment_properties(get_segment_ids(fake_volume))
@@ -66,9 +73,7 @@ def create_mesh(animal, limit):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
     parser.add_argument('--animal', help='Enter the animal', required=True)
-    parser.add_argument('--limit', help='Enter the chunk', required=False, default=0)
     args = parser.parse_args()
     animal = args.animal
-    limit = int(args.limit)
-    create_mesh(animal, limit)
+    create_mesh(animal)
 
