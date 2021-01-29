5c3ff1594fb08838315a7da4996fdcce9186dec5
Eddy O'Donnell <eddyod@yahoo.com>
2021-01-29 07:21:08 -0800

    Added lower right quadrant to fix brightness

--- fixes/fix_brightness.py
+++ fixes/fix_brightness.py
@@ -9,22 +9,37 @@ from skimage import io
 import numpy as np
 import cv2
 
+def change_lower_right(img, brightness):
+    med = np.median(img[img > 0])
+    endr, endc = img.shape
+    midr = endr // 2
+    midc = endc // 2
+    a = img.copy()
+    b = a[midr:endr,midc:endc]
+    b[b > brightness] = med
+    return a
+
+
 def change_brightness(filename, brightness, fix):
     img = io.imread(filename)
 
     if fix:
-        OUTPUT = 'fixed.tif'
-        img[img > brightness] = np.median(img[img > 0])
-        cv2.imwrite(OUTPUT, img)
+        fixed = change_lower_right(img, brightness)
+        cv2.imwrite('fixed.tif', fixed)
 
 
     else:
         levels = [6,7,8,9,9.5,9.99]
-        print('mean', np.mean(img[img > 0]))
-        print('median', np.median(img[img > 0]))
+        endr, endc = img.shape
+        midr = endr // 2
+        midc = endc // 2
+        a = img.copy()
+        lower_right = a[midr:endr, midc:endc]
+        print('mean', np.mean(lower_right[lower_right > 0]))
+        print('median', np.median(lower_right[lower_right > 0]))
         for l in levels:
             l *= 0.1
-            print(round(l,2), np.quantile(img, l))
+            print(round(l,2), np.quantile(lower_right, l))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
