0943b79c69b7592349951f811c0190ba25e05195
Eddy O'Donnell <eddyod@yahoo.com>
2021-01-29 09:04:50 -0800

    updated roi in fix brightness

--- fixes/fix_brightness.py
+++ fixes/fix_brightness.py
@@ -9,47 +9,54 @@ from skimage import io
 import numpy as np
 import cv2
 
-def change_lower_right(img, brightness):
+def change_roi(img, brightness, area):
     med = np.median(img[img > 0])
+    mn = np.mean(img[img > 0])
+    a = img.copy()
+    b = get_roi(a, area)
+    b[b > brightness] = mn + 150
+    return a
+
+def get_roi(img, area):
     endr, endc = img.shape
     midr = endr // 2
     midc = endc // 2
-    a = img.copy()
-    b = a[midr:endr,midc:endc]
-    b[b > brightness] = med
-    return a
+    if 'lr' in area:
+        b = img[midr:endr,midc:endc]
+    else:
+        b = img[0:midr, 0:endc]
+    return b
 
 
-def change_brightness(filename, brightness, fix):
+def change_brightness(filename, brightness, fix, area):
     img = io.imread(filename)
 
     if fix:
-        fixed = change_lower_right(img, brightness)
+        fixed = change_roi(img, brightness, area)
         cv2.imwrite('fixed.tif', fixed)
 
 
     else:
         levels = [6,7,8,9,9.5,9.99]
-        endr, endc = img.shape
-        midr = endr // 2
-        midc = endc // 2
         a = img.copy()
-        lower_right = a[midr:endr, midc:endc]
-        print('mean', np.mean(lower_right[lower_right > 0]))
-        print('median', np.median(lower_right[lower_right > 0]))
+        roi = get_roi(a, area)
+        print('mean', np.mean(roi[roi > 0]))
+        print('median', np.median(roi[roi > 0]))
         for l in levels:
             l *= 0.1
-            print(round(l,2), np.quantile(lower_right, l))
+            print(round(l,2), np.quantile(roi, l))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
     parser.add_argument('--file', help='Enter the animal animal', required=True)
     parser.add_argument('--fix', help='Enter channel', required=True)
     parser.add_argument('--brightness', help='Enter channel', required=True)
+    parser.add_argument('--area', help='Enter area {ul,lr}', required=False, default='lr')
 
     args = parser.parse_args()
     filename = args.file
     fix = bool({'true': True, 'false': False}[args.fix.lower()])
     brightness = int(args.brightness)
+    area = args.area
 
-    change_brightness(filename, brightness, fix)
+    change_brightness(filename, brightness, fix, area)
