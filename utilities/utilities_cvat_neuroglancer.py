ca7fe0a0349cedefa21a5ea5a0651cc76b1b29fa
Eddy O'Donnell <eddyod@yahoo.com>
2021-05-15 12:28:13 -0700

    Adjusted cpus down to 4

--- utilities/utilities_cvat_neuroglancer.py
+++ utilities/utilities_cvat_neuroglancer.py
@@ -31,8 +31,8 @@ def get_cpus():
     usecpus = (4,4)
     cpus = {}
     cpus['muralis'] = (16,40)
-    cpus['basalis'] = (12,12)
-    cpus['ratto'] = (10,10)
+    cpus['basalis'] = (4,12)
+    cpus['ratto'] = (4,10)
     hostname = get_hostname()
     if hostname in cpus.keys():
         usecpus = cpus[hostname]
