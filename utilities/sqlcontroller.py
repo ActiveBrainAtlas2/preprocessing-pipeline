4eb5ab4381cc39103c4280d59d312e3fe90ee6f4
qobilidop <qobilidop@gmail.com>
2021-05-20 12:13:41 -0700

    Update alignment error analysis with corrected data

--- utilities/sqlcontroller.py
+++ utilities/sqlcontroller.py
@@ -365,17 +365,17 @@ class SqlController(object):
             # invalid state is removed.
             self.session.close()
 
-    def get_centers_dict(self, prep_id, input_type='manual', person_id=2):
-        #get_centers_dict(animal, input_type=input_type, person_id=person_id)
-        rows = self.session.query(CenterOfMass).filter(
-            CenterOfMass.active.is_(True)).filter(CenterOfMass.prep_id == prep_id)\
-            .filter(CenterOfMass.input_type == input_type)\
-            .filter(CenterOfMass.person_id == person_id).all()
+    def get_centers_dict(self, prep_id, input_type_id=1, person_id=2):
+        rows = self.session.query(CenterOfMass)\
+            .filter(CenterOfMass.active.is_(True))\
+            .filter(CenterOfMass.prep_id == prep_id)\
+            .filter(CenterOfMass.input_type_id == input_type_id)\
+            .filter(CenterOfMass.person_id == person_id)\
+            .all()
         row_dict = {}
         for row in rows:
             structure = row.structure.abbreviation
             row_dict[structure] = [row.x, row.y, row.section]
-
         return row_dict
 
     def get_point_dataframe(self, id):
