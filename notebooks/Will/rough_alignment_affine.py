81766fda67b3a4ec7e3a9c5f8bf0229c5d347dda
madwilliam5225 <madwilliam5225@gmail.com>
2021-06-10 16:00:12 -0700

    added script for find affine transform for rough alignment

---
+++ notebooks/Will/rough_alignment_affine.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import SimpleITK as sitk
+import toolbox.sitk
+
+def get_fixed_and_moving_image(fixed_brain,moving_brain):
+    thumb_spacing = (10.4, 10.4, 20.0)
+    data_dir = Path('/net/birdstore/Active_Atlas_Data/data_root/pipeline_data')
+    moving_image_thumbnail_dir = data_dir / moving_brain / 'preps/CH1/thumbnail_aligned'
+    fixed_image_thumbnail_dir = data_dir / fixed_brain / 'preps/CH1/thumbnail_aligned'
+    moving_image_16_bit = toolbox.sitk.load_image_dir(moving_image_thumbnail_dir, spacing=thumb_spacing)
+    fixed_image_16_bit = toolbox.sitk.load_image_dir(fixed_image_thumbnail_dir, spacing=thumb_spacing)
+    moving_image = sitk.Cast(moving_image_16_bit, sitk.sitkFloat32)
+    fixed_image = sitk.Cast(fixed_image_16_bit, sitk.sitkFloat32)
+    return moving_image,fixed_image
+
+def get_initial_transform_to_align_image_centers(fixed_image, moving_image):
+    centering_transform = sitk.CenteredTransformInitializer(
+        fixed_image, moving_image,
+        sitk.AffineTransform(3),
+        sitk.CenteredTransformInitializerFilter.GEOMETRY)
+    return centering_transform
+
+def set_mutual_information_as_similarity_metic(registration_method):
+    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+    registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+    registration_method.SetMetricSamplingPercentage(0.01)
+
+def set_optimizer(registration_method):
+    registration_method.SetOptimizerAsGradientDescent(
+        learningRate=1.0,
+        numberOfIterations=100,
+        convergenceMinimumValue=1e-6,
+        convergenceWindowSize=10
+    )
+    registration_method.SetOptimizerScalesFromPhysicalShift()
+
+def set_centering_transform_as_initial_starting_point(registration_method,centering_transform):
+    affine_transform = sitk.AffineTransform(centering_transform)
+    registration_method.SetInitialTransform(affine_transform)
+    return affine_transform
+
+def init_regerstration_method():
+    registration_method = sitk.ImageRegistrationMethod()
+    registration_method.SetInterpolator(sitk.sitkLinear)
+    return registration_method
+
+def set_multi_resolution_parameters(registration_method):
+    registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
+    registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
+    registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+
+def get_affine_transform_that_aligns_images(fixed_image, moving_image,transform):
+    registration_method = init_regerstration_method()
+    set_mutual_information_as_similarity_metic(registration_method)
+    set_optimizer(registration_method)
+    set_centering_transform_as_initial_starting_point(registration_method, transform)
+    set_multi_resolution_parameters(registration_method)
+    registration_method.Execute(fixed_image, moving_image)
+
+def rough_alignment_affine(moving_brain = 'DK52',fixed_brain = 'DK43'):
+    moving_image,fixed_image = get_fixed_and_moving_image(fixed_brain,moving_brain)
+    transform = get_initial_transform_to_align_image_centers(fixed_image, moving_image)
+    get_affine_transform_that_aligns_images(fixed_image, moving_image,transform)
+    return transform
