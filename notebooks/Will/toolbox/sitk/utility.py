4216d0011e7aef0902bdf961b22621aa7e76f168
madwilliam5225 <madwilliam5225@gmail.com>
2021-06-15 17:09:05 -0700

    uodates

--- notebooks/Will/toolbox/sitk/utility.py
+++ notebooks/Will/toolbox/sitk/utility.py
@@ -22,15 +22,17 @@ def get_initial_transform_to_align_image_centers(fixed_image, moving_image):
         sitk.CenteredTransformInitializerFilter.GEOMETRY)
     return centering_transform
 
-def get_fixed_and_moving_image(fixed_brain,moving_brain):
+def get_image_from_one_brain(brain_id):
     thumb_spacing = (10.4, 10.4, 20.0)
     data_dir = Path('/net/birdstore/Active_Atlas_Data/data_root/pipeline_data')
-    moving_image_thumbnail_dir = data_dir / moving_brain / 'preps/CH1/thumbnail_aligned'
-    fixed_image_thumbnail_dir = data_dir / fixed_brain / 'preps/CH1/thumbnail_aligned'
-    moving_image_16_bit = load_image(moving_image_thumbnail_dir, spacing=thumb_spacing)
-    fixed_image_16_bit = load_image(fixed_image_thumbnail_dir, spacing=thumb_spacing)
-    moving_image = sitk.Cast(moving_image_16_bit, sitk.sitkFloat32)
-    fixed_image = sitk.Cast(fixed_image_16_bit, sitk.sitkFloat32)
+    image_thumbnail_dir = data_dir / brain_id / 'preps/CH1/thumbnail_aligned'
+    image_16_bit = load_image(image_thumbnail_dir, spacing=thumb_spacing)
+    image = sitk.Cast(image_16_bit, sitk.sitkFloat32)
+    return image
+
+def get_fixed_and_moving_image(fixed_brain,moving_brain):
+    moving_image = get_image_from_one_brain(moving_brain)
+    fixed_image = get_image_from_one_brain(fixed_brain)
     return moving_image,fixed_image
 
 def get_3d_test_grid():
