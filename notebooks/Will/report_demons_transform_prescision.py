f089299228f5d5ace041515448f818f4dee29486
madwilliam5225 <madwilliam5225@gmail.com>
2021-06-16 18:16:42 -0700

    update

--- notebooks/Will/report_demons_transform_prescision.py
+++ notebooks/Will/report_demons_transform_prescision.py
@@ -1,3 +1,7 @@
-from notebooks.Will.toolbox.plotting.plot_transformation_prescision import prepare_table_for_plot,plot
-df = prepare_table_for_plot()
-fig = plot(df, -1000, 1000, 100, 'Rigid Alignment Error After Correction')
+from notebooks.Will.toolbox.IOs.get_center_of_mass import get_atlas_centers_in_physical_coord
+from notebooks.Will.toolbox.IOs.get_calculated_transforms import get_demons_transform
+from notebooks.Will.toolbox.IOs.get_specimen_lists import get_list_of_brains_to_align
+brains_to_align = get_list_of_brains_to_align()
+atlas_coord_phys = get_atlas_centers_in_physical_coord()
+for braini in brains_to_align:
+    demons_transform = get_demons_transform(braini)
