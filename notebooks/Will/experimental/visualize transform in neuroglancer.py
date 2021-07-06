5356d4949f7cc5dcd0b729a9f286c797cbd1b6dd
madwilliam5225 <madwilliam5225@gmail.com>
2021-07-05 17:32:29 -0700

    updates

--- notebooks/Will/experimental/visualize transform in neuroglancer.py
+++ notebooks/Will/experimental/visualize transform in neuroglancer.py
@@ -1,6 +1,4 @@
-#%%
-# from notebooks.Will.toolbox.brain_lists import get_prep_list_for_rough_alignment_test
-# from notebooks.Will.toolbox import com_dict_tools
+#%%imports
 import sys
 import numpy as np
 sys.path.append('/home/zhw272/programming/pipeline_utility')
@@ -22,40 +20,26 @@ def add_stack(vol,viewer,name):
     with viewer.txn() as s:
         s.layers.append(name = name, layer = volume_layer)
 
-def get_arrays():
-    mov_brain = 'DK52'
-    fix_brain = 'DK39'
-    moving_image = load_stack_from_prepi(mov_brain)
-    fixed_image = load_stack_from_prepi(fix_brain)
-    affine_transform = get_affine_transform(fix_brain)
-    affine_transformed_image = sitk.Resample(
-        moving_image, fixed_image, affine_transform,
-        sitk.sitkLinear, 0.0, moving_image.GetPixelID())
-    fixed_arr = sitk.GetArrayViewFromImage(fixed_image)
-    moving_arr = sitk.GetArrayViewFromImage(moving_image)
-    affine_transformed_arr = sitk.GetArrayViewFromImage(affine_transformed_image)
-    # fixed_arr = np.swapaxes(fixed_arr, 0, 2)
-    # moving_arr = np.swapaxes(moving_arr, 0, 2)
-    # affine_transformed_arr = np.swapaxes(affine_transformed_arr, 0, 2)
-    return fixed_arr,moving_arr,affine_transformed_arr
-
-#%%
+#%%load data 
 mov_brain = 'DK52'
 fix_brain = 'DK39'
+#load the moving and fixed stack
 moving_image = load_stack_from_prepi(mov_brain)
 fixed_image = load_stack_from_prepi(fix_brain)
+#apply the affine transformation
 affine_transform = get_affine_transform(fix_brain)
 affine_transformed_image = sitk.Resample(
     moving_image, fixed_image, affine_transform,
     sitk.sitkLinear, 0.0, moving_image.GetPixelID())
+#convert the stacks into np arrays
 fixed_arr = sitk.GetArrayViewFromImage(fixed_image)
 moving_arr = sitk.GetArrayViewFromImage(moving_image)
 affine_transformed_arr = sitk.GetArrayViewFromImage(affine_transformed_image)
+#Do some conversion so that the arrays would be ready for neuroglancer
 fixed_arr = np.swapaxes(fixed_arr, 0, 2).astype('uint16')
 moving_arr = np.swapaxes(moving_arr, 0, 2).astype('uint16')
 affine_transformed_arr = np.swapaxes(affine_transformed_arr, 0, 2).astype('uint16')
-# #%%
-#%%
+#%%prepare neuroglancer layers
 dimensions=neuroglancer.CoordinateSpace(names=['x', 'y', 'z'], units='um', scales=[10.4,10.4, 20])
 fixed_layer = neuroglancer.LocalVolume(volume_type='image',
             data=fixed_arr, 
@@ -71,56 +55,7 @@ transformed_layer = neuroglancer.LocalVolume(volume_type='image',
             data=affine_transformed_arr, 
             dimensions=dimensions, 
             voxel_offset=(0, 0, 0))
-#%% compare untransformed moving to fixed
-viewer=neuroglancer.Viewer()
-with viewer.txn() as s:
-    s.layers.clear()
-    s.layers.append(name = 'fixed', layer = fixed_layer)
-    s.layers.append(name = 'moving', layer = moving_layer)
-    print(s.dimensions)
-    s.layers.append(name="com",
-                layer=neuroglancer.LocalAnnotationLayer(dimensions=dimensions,
-                annotations=[
-                    neuroglancer.PointAnnotation(
-                        id='d6704f30d2f08f1795d73bf387da7d5eec9d813f',
-                        point=[100, 100, 200]),
-                    neuroglancer.PointAnnotation(
-                        id='234',
-                        point=[100, 100, 300])
-                ]))
-
-print(viewer)
-#%% compare transformed moving to fixed
-viewer=neuroglancer.Viewer()
-with viewer.txn() as s:
-    s.layers.clear()
-    s.layers.append(name = 'fixed', layer = fixed_layer)
-    s.layers.append(name = 'transformed', layer = transformed_layer)
-    print(s.dimensions)
-    s.layers.append(name="com",
-                layer=neuroglancer.LocalAnnotationLayer(dimensions=dimensions,
-                annotations=[
-                    neuroglancer.PointAnnotation(
-                        id='d6704f30d2f08f1795d73bf387da7d5eec9d813f',
-                        point=[100, 100, 200]),
-                    neuroglancer.PointAnnotation(
-                        id='234',
-                        point=[100, 100, 300])
-                ]))
-
-print(viewer)
-
-#%%
-add_stack(fixed_arr,viewer,'prepi')
-#%%
-print(viewer)
-
-# %%
-plt.imshow(fixed_arr[:,:,200])
-
-# %%
-# neuroglancer = NumpyToNeuroglancer(animal='DK52',volume = moving_arr,scales=[325,325,20000],layer_type='image',data_type=moving_arr.dtype)
-# %%
+# %% get original and transformed coms
 prepid = 'DK39'
 prep_list = get_prep_list_for_rough_alignment_test()
 prepi = prep_list.index(prepid)
@@ -137,7 +72,7 @@ affine_transformed_coms_itk,affine_aligned_coms_itk = get_itk_affine_transformed
 affine_transformed_com_itk_prepi = convert_com_dict_units(
     affine_transformed_coms_itk[prepi],physical_to_thumbnail)
 transformed_coms_airlab,aligned_coms_airlab = get_airlab_transformed_coms()
-#%% compare transformed moving to fixed
+#%% functions to add annotation layer
 def get_annotations(com_dict):
     n_annotations = len(com_dict)
     names = list(com_dict.keys())
@@ -147,17 +82,9 @@ def get_annotations(com_dict):
         point_annotation = neuroglancer.PointAnnotation(id=names[annotationi],point=coordinates[annotationi])
         annotations.append(point_annotation)
     return annotations
-
-def get_annotation_properties(com_dict,color='yellow'):
-    n_annotations = len(com_dict)
-    annotation_properties = []
-    for annotationi in range(n_annotations):
-        annotation_propperty = neuroglancer.AnnotationPropertySpec(id='color',type='rgb')
-        annotation_properties.append(annotation_propperty)
-    return annotation_properties
 #%%
 viewer1=neuroglancer.Viewer()
-#%%
+#%%compare moving(DK52) to fixed(DKXX)
 with viewer1.txn() as s:
     s.layers.clear()
     s.layers.append(name = 'fixed', layer = fixed_layer)
@@ -176,7 +103,7 @@ with viewer1.txn() as s:
 print(viewer1)
 #%%
 viewer2=neuroglancer.Viewer()
-#%%
+#%%compare transformed moving(DK52+affine_transform) to fixed(DKXX)
 with viewer2.txn() as s:
     s.layers.clear()
     s.layers.append(name = 'fixed', layer = fixed_layer)
@@ -195,7 +122,7 @@ with viewer2.txn() as s:
 print(viewer2)
 #%%
 viewer3=neuroglancer.Viewer()
-#%%
+#%%compare transformed moving (DK52+affine_transform) to moving (DK52)
 with viewer3.txn() as s:
     s.layers.clear()
     s.layers.append(name = 'transformed', layer = transformed_layer)
@@ -212,5 +139,3 @@ with viewer3.txn() as s:
                 annotationColor = "#89EBFB"))
     
 print(viewer3)
-
-# %%
