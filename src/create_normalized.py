12812eac7da5f1cfda0fb32ede0af874c2247f27
Eddy O'Donnell <eddyod@yahoo.com>
2021-07-06 18:22:41 -0700

    Fixed create_normalized script

--- src/create_normalized.py
+++ src/create_normalized.py
@@ -12,55 +12,31 @@ from skimage import io
 from tqdm import tqdm
 from lib.file_location import FileLocationManager
 from lib.sqlcontroller import SqlController
-from lib.utilities_mask import rotate_image, equalized
-
-
-def fix_image(infile, rotation, flip):
-    """
-    This method does an adaptive histogram equalization and rotates if necessary.
-    :param infile: file path of image
-    :param logger: logger to keep track of things
-    :param rotation: amount of rotation. 1 = rotate by 90degrees
-    :param flip: either flip or flop
-    :return: normalized and rotated image
-    """
-    try:
-        img = io.imread(infile)
-    except:
-        print(f'Could not open {infile}')
-
-    fixed = equalized(img)
-
-    if rotation > 0:
-        fixed = rotate_image(fixed, infile, rotation)
-
-    if flip == 'flip':
-        fixed = np.flip(fixed)
-    if flip == 'flop':
-        fixed = np.flip(fixed, axis=1)
-
-    return fixed
+from lib.utilities_mask import equalized
 
 
 def create_normalization(animal, channel):
     fileLocationManager = FileLocationManager(animal)
     INPUT = fileLocationManager.thumbnail
-    CLEANED = os.path.join(fileLocationManager.prep, f'CH{channel}', 'normalized')
-    os.makedirs(CLEANED, exist_ok=True)
+    OUTPUT = os.path.join(fileLocationManager.prep, f'CH{channel}', 'normalized')
+    os.makedirs(OUTPUT, exist_ok=True)
     sqlController = SqlController(animal)
-    rotation = sqlController.scan_run.rotation
-    flip = sqlController.scan_run.flip
 
     files = sorted(os.listdir(INPUT))
 
     for file in tqdm(files):
         infile = os.path.join(INPUT, file)
-        outpath = os.path.join(CLEANED, file)
+        outpath = os.path.join(OUTPUT, file)
         if os.path.exists(outpath):
             continue
 
-        fixed = fix_image(infile, rotation, flip)
-        cv2.imwrite(outpath, fixed)
+        img = io.imread(infile)
+
+        if img.dtype == np.uint16:
+            img = (img/256).astype(np.uint8)
+
+        fixed = equalized(img)
+        cv2.imwrite(outpath, fixed.astype(np.uint8))
 
     # set task as completed
     print('Finished')
