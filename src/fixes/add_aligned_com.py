5870ee1c936dab4c78f77fe30f306dfeadbf238f
Edward O'Donnell <eddyod@yahoo.com>
2021-07-30 15:57:43 -0700

    Added abakit to add aligned com

--- src/fixes/add_aligned_com.py
+++ src/fixes/add_aligned_com.py
@@ -14,7 +14,7 @@ import numpy as np
 import os
 import sys
 from datetime import datetime
-
+from abakit.registration.algorithm import brain_to_atlas_transform, atlas_to_brain_transform, umeyama
 
 HOME = os.path.expanduser("~")
 DIR = os.path.join(HOME, 'programming/pipeline_utility/src')
@@ -23,6 +23,9 @@ from model.structure import Structure
 from model.layer_data import LayerData
 from model.scan_run import ScanRun
 from sql_setup import session
+
+
+
 CORRECTED = 2
 
 """
@@ -34,41 +37,6 @@ CORRECTED = 2
         brain_coord_phys = r_inv @ atlas_coord_phys - r_inv @ t_phys
 """
 
-
-def atlas_to_brain_transform(atlas_coord, r, t):
-    """
-    Takes an x,y,z brain coordinates, and a rotation matrix and translation vector.
-    Returns the point in atlas coordinates in micrometers.
-    params:
-        atlas_coord: tuple of x,y,z coordinates of the atlas in micrometers
-        r: float of the rotation matrix
-        t: vector of the translation matrix
-    """
-    # Bring atlas coordinates to physical space
-    atlas_coord = np.array(atlas_coord).reshape(3, 1) # Convert to a column vector
-    # Apply affine transformation in physical space
-    r_inv = np.linalg.inv(r)
-    #####brain_coord_phys = r_inv @ atlas_coord - r_inv @ t
-    brain_coord_phys = r_inv @ atlas_coord - (r_inv @  t)
-    # Bring brain coordinates back to brain space
-    #brain_coord = np.linalg.inv(brain_scale) @ brain_coord_phys
-    return brain_coord_phys.T[0] # Convert back to a row vector
-
-
-def brain_to_atlas_transform(brain_coord, r, t):
-    """
-    Takes an x,y,z brain coordinates, and a rotation matrix and translation vector.
-    params:
-        atlas_coord: tuple of x,y,z coordinates of the atlas in micrometers
-        r: float of the rotation matrix
-        t: vector of the translation matrix
-    Returns the point in atlas coordinates in micrometers.
-    """
-    # Transform brain coordinates to physical space
-    brain_coord = np.array(brain_coord).reshape(3, 1) # Convert to a column vector
-    atlas_coord = r @ brain_coord + t
-    #####return atlas_coord_phys.T[0] # Convert back to a row vector
-    return atlas_coord.T[0] # Convert back to a row vector
 
 
 def get_centers(animal, input_type_id, person_id=2):
@@ -128,44 +96,6 @@ def get_common_structure(brains):
     return common_structures
 
 
-def align_point_sets(src, dst, with_scaling=True):
-    """
-    Analytically computes a transformation that minimizes the squared error between source and destination.
-    ------------------------------------------------------
-    src is the dictionary of the brain we want to align
-    dst is the dictionary of the atlas structures
-    Defaults to scaling true, which means the transformation is rigid and a uniform scale.
-    returns the linear transformation r, and the translation vector t
-    """
-    assert src.shape == dst.shape
-    assert len(src.shape) == 2
-    m, n = src.shape  # dimension, number of points
-
-    src_mean = np.mean(src, axis=1).reshape(-1, 1)
-    dst_mean = np.mean(dst, axis=1).reshape(-1, 1)
-
-    src_demean = src - src_mean
-    dst_demean = dst - dst_mean
-
-    u, s, vh = np.linalg.svd(dst_demean @ src_demean.T / n)
-
-    # deal with reflection
-    e = np.ones(m)
-    if np.linalg.det(u) * np.linalg.det(vh) < 0:
-        print('reflection detected')
-        e[-1] = -1
-
-    r = u @ np.diag(e) @ vh
-
-    if with_scaling:
-        src_var = (src_demean ** 2).sum(axis=0).mean()
-        c = sum(s * e) / src_var
-        r *= c
-
-    t = dst_mean - r @ src_mean
-    return r, t
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
     parser.add_argument('--pointbrain', help='Enter point animal', required=True)
@@ -186,14 +116,14 @@ if __name__ == '__main__':
     point_structures = sorted(pointdata.keys())
     dst_point_set = np.array([atlas_centers[s] for s in point_structures if s in common_structures]).T
     point_set = np.array([pointdata[s] for s in point_structures if s in common_structures]).T
-    r0, t0 = align_point_sets(point_set, dst_point_set)
+    r0, t0 = umeyama(point_set, dst_point_set)
 
 
     imagedata = get_centers(imagebrain, CORRECTED)
     image_structures = sorted(imagedata.keys())
     image_set = np.array([imagedata[s] for s in image_structures if s in common_structures]).T
     dst_point_set = np.array([atlas_centers[s] for s in image_structures if s in common_structures]).T
-    r1, t1 = align_point_sets(image_set, dst_point_set)
+    r1, t1 = umeyama(image_set, dst_point_set)
 
     # get some COM data from DK55
     for abbrev, coord in pointdata.items():
