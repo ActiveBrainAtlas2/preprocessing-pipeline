53e177c526c963e972e9ac4248b1cf21ce00f990
madwilliam5225 <madwilliam5225@gmail.com>
2021-10-05 13:50:54 -0700

    updates

--- notebooks/Will/add_cell_data/test.py
+++ notebooks/Will/add_cell_data/test.py
@@ -4,7 +4,7 @@ from cell_model import Cell
 controller = FeaturesController()
 test_array  =np.random.rand(10)
 test_array_bytes = test_array.tobytes()
-cell = Cell(id = 1,prep_id = 'test',section = 1,x = 1, y = 1,cell_images = test_array_bytes)
+cell = Cell(id = 1,prep_id = 'test',section = 1,x = 1, y = 1,cell_images = test_array_bytes,image_width = 5)
 controller.drop_cell_by_id(1)
 controller.add_row(cell)
 celli = controller.get_cell_by_id(1)
