b521eeaddc1dfc4d1d1301bfb376ea5b762a1f23
Eddy O'Donnell <eddyod@yahoo.com>
2021-10-11 07:50:33 -0400

    Added try except to the process image method in create neuroglancer image

--- src/lib/utilities_cvat_neuroglancer.py
+++ src/lib/utilities_cvat_neuroglancer.py
@@ -302,9 +302,21 @@ class NumpyToNeuroglancer():
         if completed:
             print(f"Section {index} already processed, skipping ")
             return
-        img = io.imread(infile, img_num=0)
-        img = img.reshape(self.num_channels, img.shape[0], img.shape[1]).T
-        self.precomputed_vol[:, :, index] = img
+        try:
+            img = io.imread(infile, img_num=0)
+        except IOError as ioe:
+            print(f'could not open {infile} {ioe}')
+            return
+        try:
+            img = img.reshape(self.num_channels, img.shape[0], img.shape[1]).T
+        except:
+            print(f'could not reshape {infile}')
+            return
+        try:
+            self.precomputed_vol[:, :, index] = img
+        except:
+            print(f'could not set {infile} to precomputed')
+            return
         set_file_completed(self.animal, self.progress_id, basefile)
         del img
         return
