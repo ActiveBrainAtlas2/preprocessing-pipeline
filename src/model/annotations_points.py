c43cc2a0c65232a5a1ccd820ec7f7e3d617b87e3
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-03-18 12:49:25 +0700

    Added a good notebook for fetching contour polygons

---
+++ src/model/annotations_points.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.sql.sqltypes import Float
+from .atlas_model import Base, AtlasModel
+from model.brain_region import BrainRegion
+
+
+
+class AnnotationPoint(Base):
+    __tablename__ = 'annotations_points'
+    id =  Column(Integer, primary_key=True, nullable=False)
+    prep_id = Column(String, nullable=False)
+    FK_input_id = Column(Integer)
+    FK_owner_id = Column(Integer)
+    FK_structure_id = Column(Integer, ForeignKey('structure.id'), nullable=True)
+    label = Column(String, nullable=False)
+    segment_id = Column(String, nullable=True)
+    x = Column(Float, nullable=False)
+    y = Column(Float, nullable=False)
+    z = Column(Float, nullable=False)
+
+    brain_region = relationship('BrainRegion', lazy=True)
+
+
+
+
+
