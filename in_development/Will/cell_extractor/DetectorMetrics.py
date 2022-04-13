6dd99656216c89d174b875c28b709b31917ded83
madwilliam <madwilliam5225@gmail.com>
2022-04-13 14:51:56 -0700

    updates

--- in_development/Will/cell_extractor/DetectorMetrics.py
+++ in_development/Will/cell_extractor/DetectorMetrics.py
@@ -9,35 +9,18 @@ import pickle as pk
 from cell_extractor.CellDetectorBase import CellDetectorBase
 
 
-class DetectorMetrics(CellDetectorBase):
+class AnnotationProximityTool(CellDetectorBase):
+    '''
+    This tool finds and groups annotations that are really close together.
+    The aim is to identify annotations of the same cell that are marked by different detector or annotatators
+    '''
 
-    def __init__(self,animal,round=1):
-        super().__init__(animal,round = round)
-        self.qc_annotation_input_path = '/scratch/programming/preprocessing-pipeline/in_development/yoav/marked_cell_detector/data2/'
+    def __init__(self,*arg,**kwarg):
+        super().__init__(*arg,**kwarg)
         self.pair_distance = 30
 
-    def load_qc_annotations(self):
-        dfs=self.load_human_qc()+self.load_machine_detection()
-        self.qc_annotations=pd.concat(dfs)
-        self.qc_annotations.columns=['x','y','section','name']
-        self.qc_annotations['x']=np.round(self.qc_annotations['x'])
-        self.qc_annotations['y']=np.round(self.qc_annotations['y'])
-        self.nannotations = len( self.qc_annotations)
-    
-    def load_human_qc(self):
-        self.annotation_category_and_filepath = {
-            'manual_train':       self.qc_annotation_input_path+'/DK55_premotor_manual_2021-12-09.csv',
-            'manual_negative':    self.qc_annotation_input_path+'/DK55_premotor_manual_negative_round1_2021-12-09.csv',
-            'manual_positive':    self.qc_annotation_input_path+'/DK55_premotor_manual_positive_round1_2021-12-09.csv'}
-        dfs=[]
-        for name,path in self.annotation_category_and_filepath.items():
-            df= pd.read_csv(path,header=None)
-            df['name']=name
-            dfs.append(df)
-        return dfs
-
     def calculate_distance_matrix(self):
-        df = self.qc_annotations.copy()
+        df = self.annotations_to_compare.copy()
         df['section']*=1000
         self.distances=distance_matrix(np.array(df.iloc[:,:3]),np.array(df.iloc[:,:3]))
 
@@ -48,7 +31,7 @@ class DetectorMetrics(CellDetectorBase):
         return A,A
 
     def check(self,L,include=None,exclude=None,size_min=None,size_max=None):
-        """ Return true if names in yes appear in L, names in no do not appear, 
+        """ Return true if names in include appear in L, names in exclude do not appear in L, 
             and the length of the list is between min_size and max_size"""
         if not include is None:
             for e in include:
@@ -72,8 +55,8 @@ class DetectorMetrics(CellDetectorBase):
         self.close_pairs=close_pairs[close_pairs[:,0]< close_pairs[:,1],:]
         self.npairs = len(self.close_pairs)
     
-    def find_set_of_points_that_are_close_to_each_other(self):
-        def find_all_pairs_that_are_close():
+    def group_and_label_close_pairs(self):
+        def group_close_paris():
             self.pairs={}
             for i in range(self.nannotations):
                 self.pairs[i]=[i]
@@ -81,7 +64,7 @@ class DetectorMetrics(CellDetectorBase):
                 first,second=self.close_pairs[i]
                 self.pairs[first],self.pairs[second]=self.find_union(self.pairs[first],self.pairs[second])
         def find_category_of_close_pairs():
-            categories=list(self.qc_annotations.iloc[:,-1])
+            categories=list(self.annotations_to_compare.iloc[:,-1])
             self.pair_categories={}
             for key in self.pairs:
                 self.pair_categories[key]=[categories[i] for i in self.pairs[key]]
@@ -92,7 +75,7 @@ class DetectorMetrics(CellDetectorBase):
                 for j in self.pairs[i]:
                     if j != i and j in self.pairs:
                         del self.pairs[j]
-        find_all_pairs_that_are_close()
+        group_close_paris()
         print('before removing duplicates',len(self.pairs))
         remove_duplicate_pairs()
         print('after removing duplicates',len(self.pairs))
@@ -101,23 +84,74 @@ class DetectorMetrics(CellDetectorBase):
     def get_pairs_in_category(self,pair_category_criteria = lambda x : True):
         is_category = []
         for i in self.pairs:
-            annotationi = self.qc_annotations.iloc[i,:]
+            annotationi = self.annotations_to_compare.iloc[i,:]
             if pair_category_criteria(self.pair_categories[i]):
                 is_category.append((i,annotationi))
         return is_category
 
-    def calculate_and_save_qualifications(self):
-        self.load_qc_annotations()
+    def print_quantification(self,quantification):
+        for keyi in quantification:
+            print(keyi)
+            print(len(quantification[keyi]))
+    
+    def set_annotations_to_compare(self,annotations):
+        '''annotations_to_compare should be a dataframe with the columns:
+           x:       x coordinate, float
+           y:       y coordinate, float
+           section: section number, int/float
+           name:    category of the annotation. str
+        '''
+        self.annotations_to_compare = annotations
+
+
+class DetectorMetricsDK55Round1(AnnotationProximityTool):
+    def __init__(self,animal = 'DK55', *args,**kwrds):
+        super().__init__(animal,*args,**kwrds)
+        self.qc_annotation_input_path = '/scratch/programming/preprocessing-pipeline/in_development/yoav/marked_cell_detector/data2/'
+
+    def load_annotations_to_compare(self):
+        dfs=self.load_human_qc()+self.load_machine_detection()
+        self.annotations_to_compare=pd.concat(dfs)
+        self.annotations_to_compare.columns=['x','y','section','name']
+        self.annotations_to_compare['x']=np.round(self.annotations_to_compare['x'])
+        self.annotations_to_compare['y']=np.round(self.annotations_to_compare['y'])
+        self.nannotations = len( self.annotations_to_compare)
+    
+    def load_human_qc(self):
+        self.annotation_category_and_filepath = {
+            'manual_train':       self.qc_annotation_input_path+'/DK55_premotor_manual_2021-12-09.csv',
+            'manual_negative':    self.qc_annotation_input_path+'/DK55_premotor_manual_negative_round1_2021-12-09.csv',
+            'manual_positive':    self.qc_annotation_input_path+'/DK55_premotor_manual_positive_round1_2021-12-09.csv'}
+        dfs=[]
+        for name,path in self.annotation_category_and_filepath.items():
+            df= pd.read_csv(path,header=None)
+            df['name']=name
+            dfs.append(df)
+        return dfs
+    
+    def load_machine_detection(self):
+        self.machine_detection_filepath ={
+            'computer_sure':      self.qc_annotation_input_path+'/DK55_premotor_sure_detection_2021-12-09.csv',
+            'computer_unsure':    self.qc_annotation_input_path+'/DK55_premotor_unsure_detection_2021-12-09.csv'}
+        dfs=[]
+        for name,path in self.machine_detection_filepath.items():
+            df= pd.read_csv(path,header=None)
+            df['name']=name
+            dfs.append(df)
+        return dfs
+    
+    def calculate_and_save_quantification(self):
+        self.load_annotations_to_compare()
         self.calculate_distance_matrix()
         self.find_close_pairs()
-        self.find_set_of_points_that_are_close_to_each_other()
-        self.get_train_section_qualification()
-        self.get_all_section_qualifications()
-        self.print_qualifications(self.all_section_quantifications)
-        self.print_qualifications(self.train_section_quantifications)
-        pk.dump((self.all_section_quantifications,self.train_section_quantifications),open(self.QUALIFICATIONS,'wb'))
+        self.group_and_label_close_pairs()
+        self.get_train_section_quantification()
+        self.get_all_section_quantification()
+        self.print_quantification(self.all_section_quantifications)
+        self.print_quantification(self.train_section_quantifications)
+        pk.dump((self.all_section_quantifications,self.train_section_quantifications),open(self.quantification,'wb'))
 
-    def get_train_section_qualification(self):
+    def get_train_section_quantification(self):
         self.train_section_quantifications={}
         category_name_and_criteria = {
             'Computer Detected, Human Missed'           :lambda pair_categories : self.check(pair_categories,include=['computer_sure'],size_max=1),
@@ -127,7 +161,7 @@ class DetectorMetrics(CellDetectorBase):
         for category,criteria in category_name_and_criteria.items():
             self.train_section_quantifications[category] = self.get_pairs_in_category(pair_category_criteria = criteria)
     
-    def get_all_section_qualifications(self):
+    def get_all_section_quantification(self):
         self.all_section_quantifications = {}
         category_name_and_criteria = {
             'computer missed, human detected'   :lambda pair_categories : self.check(pair_categories,include=['manual_positive'],exclude=['computer_sure','computer_unsure'],size_max=1),
@@ -142,28 +176,7 @@ class DetectorMetrics(CellDetectorBase):
         for category,criteria in category_name_and_criteria.items():
             self.all_section_quantifications[category] = self.get_pairs_in_category(pair_category_criteria = criteria)
 
-    def print_qualifications(self,qualifications):
-        for keyi in qualifications:
-            print(keyi)
-            print(len(qualifications[keyi]))
-
-
-class DetectorMetricsDK55Round1(DetectorMetrics):
-    def __init__(self,*args,**kwrds):
-        super().__init__(*args,**kwrds)
-    
-    def load_machine_detection(self):
-        self.machine_detection_filepath ={
-            'computer_sure':      self.qc_annotation_input_path+'/DK55_premotor_sure_detection_2021-12-09.csv',
-            'computer_unsure':    self.qc_annotation_input_path+'/DK55_premotor_unsure_detection_2021-12-09.csv'}
-        dfs=[]
-        for name,path in self.machine_detection_filepath.items():
-            df= pd.read_csv(path,header=None)
-            df['name']=name
-            dfs.append(df)
-        return dfs
-
 
 if __name__=='__main__':
     mec = DetectorMetricsDK55Round1('DK55',round =1)
-    mec.calculate_and_save_qualifications()
+    mec.calculate_and_save_quantification()
