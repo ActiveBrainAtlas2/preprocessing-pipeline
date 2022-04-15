6f0076bba000d0d44a00878b1a02f027d15001a4
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-04-15 14:43:14 +0700

    Added a model to store neuroglancer paths

--- src/fixes/scrape_neuroglancer.py
+++ src/fixes/scrape_neuroglancer.py
@@ -6,8 +6,9 @@ PIPELINE_ROOT = Path('./src').absolute()
 sys.path.append(PIPELINE_ROOT.as_posix())
 print(PIPELINE_ROOT.as_posix())
 from lib.sqlcontroller import SqlController
-
+from model.neuroglancer_data import AvailableNeuroglancerData
 #from lib.FileLocationManager import FileLocationManager
+from datetime import datetime
 
 
 
@@ -18,13 +19,20 @@ def fetch_animals_from_db():
 
 
 def parse_directories():
+    sqlController = SqlController('MD585')
     DATA_ROOT = '/net/birdstore/Active_Atlas_Data/data_root/pipeline_data'
     animals = fetch_animals_from_db()
+    now = datetime.now()
+
     for animal in animals:
         data_dir = os.path.join(DATA_ROOT, animal, 'neuroglancer_data')
         if os.path.exists(data_dir):
-            for d in os.listdir(data_dir):
-                print(animal, data_dir, d)
+            for description in os.listdir(data_dir):
+                url = f'https://activebrainatlas.ucsd.edu/data/{animal}/neuroglancer_data/{description}'
+                print(animal,  url, description)
+                data = AvailableNeuroglancerData(prep_id=animal, url=url, description=description, active=1, 
+                    created=now)
+                sqlController.add_row(data)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
