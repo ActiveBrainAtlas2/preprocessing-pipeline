1bc7dae1ccf3f6ac818a6d8cdf437747ac45689e
Duane Rinehart <drinehart@ucsd.edu>
2022-06-07 10:21:12 -0700

    symlink

--- src/lib/PrepCreater.py
+++ src/lib/PrepCreater.py
@@ -5,9 +5,9 @@ Image.MAX_IMAGE_PIXELS = None
 from concurrent.futures.process import ProcessPoolExecutor
 
 # from shutil import copyfile
-from abakit.lib.SqlController import SqlController
+from abakit.lib.Controllers.SqlController import SqlController
 from abakit.lib.utilities_process import test_dir, create_downsample
-from lib.pipeline_utilities import get_image_size
+from abakit.utilities.shell_tools import get_image_size
 
 
 class PrepCreater:
@@ -44,9 +44,12 @@ class PrepCreater:
             self.sqlController.update_tif(section.id, width, height)
         workers = self.get_nworkers()
 
-        os.symlink(
-            input_paths, output_paths
-        )  # Do we need multiple cores? Do we need to check filechecksums?
+        for file_key in zip(input_paths, output_paths):
+            os.symlink(*file_key)
+
+        # os.symlink(
+        #     input_paths, output_paths
+        # )  # Do we need multiple cores? Do we need to check filechecksums?
 
         # self.run_commands_in_parallel_with_executor([input_paths, output_paths], workers, copyfile)
 
