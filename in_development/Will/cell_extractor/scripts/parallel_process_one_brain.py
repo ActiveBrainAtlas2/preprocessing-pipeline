fe9a3a948bb49cab1b2231e27692301ebff5174a
madwilliam5225 <madwilliam5225@gmail.com>
2022-06-23 18:18:53 -0700

    u0dates

---
+++ in_development/Will/cell_extractor/scripts/parallel_process_one_brain.py
@@ -0,0 +1,12 @@
+from cell_extractor.ExampleFinder import create_examples_for_one_section 
+from cell_extractor.FeatureFinder import create_features_for_one_section
+from cell_extractor.CellDetector import detect_cell
+from cell_extractor.CellDetectorBase import parallel_process_all_sections
+
+
+if __name__ =='__main__':
+    animal = 'DK52'
+    for threshold in [2100,2200,2300,2700]:
+        parallel_process_all_sections(animal,create_examples_for_one_section,disk = '/net/birdstore/Active_Atlas_Data/',segmentation_threshold=threshold,njobs=7)
+        parallel_process_all_sections(animal,create_features_for_one_section,disk = '/net/birdstore/Active_Atlas_Data/',segmentation_threshold=threshold,njobs=10)
+        detect_cell(animal,round=2,segmentation_threshold=threshold)
