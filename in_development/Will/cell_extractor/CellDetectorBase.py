477128ed2074be8a244f1c768b658dab83600b69
madwilliam5225 <madwilliam5225@gmail.com>
2022-06-24 08:00:25 -0700

    cell extractor updates

--- in_development/Will/cell_extractor/CellDetectorBase.py
+++ in_development/Will/cell_extractor/CellDetectorBase.py
@@ -6,7 +6,8 @@ import pickle as pkl
 import pandas as pd
 import numpy as np
 from cell_extractor.DetectionPlotter import DetectionPlotter
-from cell_extractor.Predictor import Predictor
+from cell_extractor.Predictor import GreedyPredictor,Predictor
+from cell_extractor.Detector import Detector
 import concurrent.futures
 
 class CellDetectorBase(Brain):
@@ -31,7 +32,8 @@ class CellDetectorBase(Brain):
     def set_folder_paths(self):
         self.DATA_PATH = f"/{self.disk}/cell_segmentation/"
         self.ANIMAL_PATH = os.path.join(self.DATA_PATH,self.animal)
-        self.DETECTOR = os.path.join(self.DATA_PATH,'detectors')
+        self.DETECTOR = os.path.join(self.ANIMAL_PATH,'detectors')
+        self.MODELS = os.path.join(self.DATA_PATH,'models')
         self.FEATURE_PATH = os.path.join(self.ANIMAL_PATH,'features')
         self.DETECTION = os.path.join(self.ANIMAL_PATH,'detections')
         self.AVERAGE_CELL_IMAGE_DIR = os.path.join(self.ANIMAL_PATH,'average_cell_image.pkl')
@@ -40,15 +42,14 @@ class CellDetectorBase(Brain):
         self.CH1 = os.path.join(self.ANIMAL_PATH,"CH1")
         self.CH3_SECTION_DIR=os.path.join(self.CH3,f"{self.section:03}")
         self.CH1_SECTION_DIR=os.path.join(self.CH1,f"{self.section:03}")
-        self.ALL_FEATURES = os.path.join(self.ANIMAL_PATH,'all_features.csv')
         self.QUALIFICATIONS = os.path.join(self.FEATURE_PATH,f'categories_round{self.round}.pkl')
         self.POSITIVE_LABELS = os.path.join(self.FEATURE_PATH,f'positive_labels_for_round_{self.round}_threshold_{self.segmentation_threshold}.pkl')
         self.DETECTOR_PATH = os.path.join(self.DETECTOR,f'detector_round_{self.round}_threshold_{self.segmentation_threshold}.pkl')
         self.DETECTION_RESULT_DIR = os.path.join(self.DETECTION,f'detections_{self.animal}.{str(self.round)}_threshold_{self.segmentation_threshold}.csv')
         self.ALL_FEATURES = os.path.join(self.FEATURE_PATH,f'all_features_threshold_{self.segmentation_threshold}.csv')
-
+        self.MODEL_PATH = os.path.join(self.MODELS,f'models_from_qc_round_{self.round}_threshold_{self.segmentation_threshold}.pkl')
     def check_path_exists(self):
-        check_paths = [self.ANIMAL_PATH,self.FEATURE_PATH,self.DETECTION,self.DETECTOR]
+        check_paths = [self.ANIMAL_PATH,self.FEATURE_PATH,self.DETECTION,self.DETECTOR,self.MODELS]
         for path in check_paths:
             os.makedirs(path,exist_ok = True)
     
@@ -256,7 +257,10 @@ class CellDetectorBase(Brain):
         pkl.dump(detector,open(self.DETECTOR_PATH,'wb'))
     
     def load_detector(self):
-        detector = pkl.load(open(self.DETECTOR_PATH,'rb'))
+        models = self.load_models()
+        detector = Detector()
+        detector.model = models
+        detector.predictor = Predictor()
         return detector
     
     def save_custom_features(self,features,file_name):
@@ -309,6 +313,21 @@ class CellDetectorBase(Brain):
     def report_detection_status():
         ...
 
+    def save_models(self,models):
+        try:
+            with open(self.MODEL_PATH,'wb') as pkl_file:
+                pkl.dump(models,pkl_file)
+        except IOError as e:
+            print(e)
+    
+    def load_models(self):
+        try:
+            with open(self.MODEL_PATH,'rb') as pkl_file:
+                models = pkl.load(pkl_file)
+            return models
+        except IOError as e:
+            print(e)
+        
 def get_sections_with_annotation_for_animali(animal):
     base = CellDetectorBase(animal)
     return base.get_sections_with_csv()
@@ -343,8 +362,9 @@ def list_available_animals(disk = '/net/birdstore/Active_Atlas_Data/',has_exampl
             animals.remove(animali)
     return animals
 
-def parallel_process_all_sections(animal,processing_function,*args,njobs = 10,**kwargs):
-    sections = get_all_sections_for_animali(animal)
+def parallel_process_all_sections(animal,processing_function,*args,njobs = 10,sections=None,**kwargs):
+    if sections is None:
+        sections = get_all_sections_for_animali(animal)
     with concurrent.futures.ProcessPoolExecutor(max_workers=njobs) as executor:
         results = []
         for sectioni in sections:
--- in_development/Will/cell_extractor/Detector.py
+++ in_development/Will/cell_extractor/Detector.py
@@ -1,9 +1,10 @@
-from cell_extractor.Predictor import Predictor
+from attr import has
+from cell_extractor.Predictor import GreedyPredictor
 import numpy as np
 import xgboost as xgb
 import matplotlib.pyplot as plt
 class Detector():
-    def __init__(self,model=None,predictor:Predictor=Predictor()):
+    def __init__(self,model=None,predictor:GreedyPredictor=GreedyPredictor()):
         self.model = model
         self.predictor = predictor
 
@@ -19,31 +20,36 @@ class Detector():
         for i in range(len(self.model)):
             bst=self.model[i]
             scores[:,i] = bst.predict(all, iteration_range=[1,bst.best_ntree_limit], output_margin=True)
-        _mean=np.mean(scores,axis=1)
-        _std=np.std(scores,axis=1)
-        return scores,labels,_mean,_std
+        mean=np.mean(scores,axis=1)
+        std=np.std(scores,axis=1)
+        return scores,labels,mean,std
 
-    def get_prediction(self,_mean,_std):
+    def get_prediction(self,mean,std):
         predictions=[]
-        for mean,std in zip(_mean,_std):
+        for mean,std in zip(mean,std):
             p=self.predictor.decision(float(mean),float(std))
             predictions.append(p)
         return np.array(predictions)
+    
+    def calculate_and_set_scores(self,df):
+        if not hasattr(self,'mean') or hasattr(self,'std') or hasattr(self,'labels'):
+            _,self.labels,self.mean,self.std = self.calculate_scores(df)
 
     def plot_score_scatter(self,df):
-        scores,labels,_mean,_std = self.calculate_scores(df)
+        self.calculate_and_set_scores(df)
         plt.figure(figsize=[15,10])
-        plt.scatter(_mean,_std,c=labels,s=3)
+        plt.scatter(self.mean,self.std,c=self.labels,s=3)
         plt.title('mean and std of scores for 30 classifiers')
         plt.xlabel('mean')
         plt.ylabel('std')
         plt.grid()
     
     def plot_decision_scatter(self,features):
-        scores,labels,_mean,_std = self.calculate_scores(features)
-        predictions=self.get_prediction(_mean,_std)
+        self.calculate_and_set_scores(features)
+        if not hasattr(self,'predictions'):
+            self.predictions=self.get_prediction(self.mean,self.std)
         plt.figure(figsize=[15,10])
-        plt.scatter(_mean,_std,c=predictions+labels,s=5)
+        plt.scatter(self.mean,self.std,c=self.predictions+self.labels,s=5)
         plt.title('mean and std of scores for 30 classifiers')
         plt.xlabel('mean')
         plt.ylabel('std')
