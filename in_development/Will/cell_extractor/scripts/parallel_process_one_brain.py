14f7afec5a2797198a9458616624d43be986494a
madwilliam5225 <madwilliam5225@gmail.com>
2022-06-23 18:34:20 -0700

    upd

--- in_development/Will/cell_extractor/scripts/parallel_process_one_brain.py
+++ in_development/Will/cell_extractor/scripts/parallel_process_one_brain.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/data/preprocessing-pipeline/in_development/Will/')
 from cell_extractor.ExampleFinder import create_examples_for_one_section 
 from cell_extractor.FeatureFinder import create_features_for_one_section
 from cell_extractor.CellDetector import detect_cell
@@ -5,8 +7,8 @@ from cell_extractor.CellDetectorBase import parallel_process_all_sections
 
 
 if __name__ =='__main__':
-    animal = 'DK52'
+    animal = 'DK40'
     for threshold in [2100,2200,2300,2700]:
-        parallel_process_all_sections(animal,create_examples_for_one_section,disk = '/net/birdstore/Active_Atlas_Data/',segmentation_threshold=threshold,njobs=7)
-        parallel_process_all_sections(animal,create_features_for_one_section,disk = '/net/birdstore/Active_Atlas_Data/',segmentation_threshold=threshold,njobs=10)
+        parallel_process_all_sections(animal,create_examples_for_one_section,disk = '/scratch/',segmentation_threshold=threshold,njobs=20)
+        parallel_process_all_sections(animal,create_features_for_one_section,disk = '/scratch/',segmentation_threshold=threshold,njobs=40)
         detect_cell(animal,round=2,segmentation_threshold=threshold)
