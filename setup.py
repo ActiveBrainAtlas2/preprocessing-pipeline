bab9f1ecffe6c217e0332dccafc46b985adbae64
madwilliam5225 <madwilliam5225@gmail.com>
2022-07-12 13:34:44 -0700

    eliminating redundency

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # This is just a shim for `pip install -e` to work. See
 #   https://setuptools.readthedocs.io/en/latest/userguide/quickstart.html#development-mode
 
-import setuptools
-setuptools.setup(name = 'abakit',packages = ['abakit'], package_dir = {'abakit':'src'})
+from setuptools import setup, find_packages
+setup(name = 'abakit',packages = find_packages())
 
