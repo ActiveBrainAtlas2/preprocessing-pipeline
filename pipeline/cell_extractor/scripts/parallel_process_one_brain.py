a065f8113ad860f125f91eb0c81769b825442d66
madwilliam5225 <madwilliam5225@gmail.com>
2022-08-11 16:10:12 -0700

    downsample = False

--- pipeline/cell_extractor/scripts/parallel_process_one_brain.py
+++ pipeline/cell_extractor/scripts/parallel_process_one_brain.py
@@ -10,12 +10,12 @@ from cell_extractor.CellDetectorBase import parallel_process_all_sections,CellDe
 
 if __name__ =='__main__':
     # animal = 'DK54'
-    animal = 'DKXX'
-    for threshold in [2100,2200,2300,2700]:
+    animal = 'DK39'
+    for threshold in [2000,2100,2200,2300,2700]:
         base = CellDetectorBase(animal)
         sections = base.get_sections_without_example(threshold)
-        parallel_process_all_sections(animal,create_examples_for_one_section,disk = '/net/birdstore/Active_Atlas_Data',segmentation_threshold=threshold,sections=sections,njobs=3)
+        parallel_process_all_sections(animal,create_examples_for_one_section,disk = '/net/birdstore/Active_Atlas_Data',segmentation_threshold=threshold,sections=sections,njobs=5)
         sections = base.get_sections_without_features(threshold)
-        parallel_process_all_sections(animal,create_features_for_one_section,disk = '/net/birdstore/Active_Atlas_Data',segmentation_threshold=threshold,sections=sections,njobs=3)
+        parallel_process_all_sections(animal,create_features_for_one_section,disk = '/net/birdstore/Active_Atlas_Data',segmentation_threshold=threshold,sections=sections,njobs=5)
         detect_cell(animal,round=2,segmentation_threshold=threshold)
-    detect_cell_multithreshold(animal)
+    detect_cell_multithreshold(animal,round=2)
