a48a8c44f281a4b4569541020be8855edc736bbc
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-09-03 10:08:55 +0700

    Added a mask training tool for the TG structure

---
+++ pipeline/masking/create_polyfills_from_DB.py
@@ -0,0 +1,85 @@
+import argparse
+import os, sys
+import shutil
+import numpy as np
+from collections import defaultdict
+import cv2
+from tqdm import tqdm
+from pathlib import Path
+
+PIPELINE_ROOT = Path('./pipeline').absolute()
+sys.path.append(PIPELINE_ROOT.as_posix())
+
+from Controllers.SqlController import SqlController
+from Controllers.PolygonSequenceController import PolygonSequenceController
+from lib.FileLocationManager import FileLocationManager
+
+from utilities.utilities_process import SCALING_FACTOR
+
+def create_segmentation(animal, annotator_id, structure_id, debug=False):
+    DATA_PATH = '/net/birdstore/Active_Atlas_Data/data_root/brains_info/masks'
+    fileLocationManager = FileLocationManager(animal,)
+    sqlController = SqlController(animal)
+    # vars
+    INPUT = os.path.join(fileLocationManager.prep, 'CH1', 'thumbnail_aligned')
+    MASK_OUTPUT = os.path.join(DATA_PATH, 'tg', 'thumbnail_masked')
+    os.makedirs(MASK_OUTPUT, exist_ok=True)
+    NORM_OUTPUT = os.path.join(DATA_PATH, 'tg', 'normalized')
+    os.makedirs(NORM_OUTPUT, exist_ok=True)
+
+    polygon = PolygonSequenceController()
+    df = polygon.get_volume(animal, annotator_id, structure_id)
+    if debug:
+        print(df.head())
+        print(df.tail())
+    
+    scale_xy = sqlController.scan_run.resolution
+    z_scale = sqlController.scan_run.zresolution
+    scales = np.array([scale_xy, scale_xy, z_scale])
+    if debug:
+        print('scales', scales, SCALING_FACTOR)
+        sys.exit()
+    
+    polygons = defaultdict(list)
+    
+    for _, row in df.iterrows():
+        x = row['coordinate'][0]
+        y = row['coordinate'][1]
+        z = row['coordinate'][2]
+        xy = (x/scale_xy*SCALING_FACTOR, y/scale_xy*SCALING_FACTOR)
+        section = int(np.round(z/z_scale))
+        polygons[section].append(xy)
+        
+    color = 255
+    
+    for section, points in tqdm(polygons.items()):
+        file = str(section).zfill(3) + ".tif"
+        inpath = os.path.join(INPUT, file)
+        infile = cv2.imread(inpath, cv2.IMREAD_GRAYSCALE)
+        template = np.zeros(( infile.shape), dtype=np.uint8)
+        points = np.array(points)
+        points = points.astype(np.int32)
+        cv2.fillPoly(template, pts = [points], color = color)
+        filename = f"{animal}.{annotator_id}.{structure_id}.{file}"
+        mask_outpath = os.path.join(MASK_OUTPUT, filename)
+        norm_outpath = os.path.join(NORM_OUTPUT, filename)
+        cv2.imwrite(mask_outpath, template)
+        shutil.copyfile(inpath, norm_outpath)
+    
+                
+        
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Work on Animal')
+    parser.add_argument('--animal', help='Enter the animal', required=True)
+    parser.add_argument('--annotator_id', help='Enter the annotator_id', required=True)
+    parser.add_argument('--structure_id', help='Enter the structure_id', required=True)
+    parser.add_argument('--debug', help='Enter true or false', required=False, default='false')
+    
+
+    args = parser.parse_args()
+    animal = args.animal
+    annotator_id = int(args.annotator_id)
+    structure_id = int(args.structure_id)
+    debug = bool({'true': True, 'false': False}[str(args.debug).lower()])
+    create_segmentation(animal, annotator_id, structure_id, debug)
