22a275a4cd92ee0d3fca0f73c2435f8dde97592c
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-09-05 08:27:57 +0700

    Added the correct list of losses to a log file

--- pipeline/masking/mask_trainer.py
+++ pipeline/masking/mask_trainer.py
@@ -21,13 +21,11 @@ ROOT = '/net/birdstore/Active_Atlas_Data/data_root/brains_info/masks'
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
     parser.add_argument('--animal', help='specify animal', required=False)
-    parser.add_argument('--runmodel', help='run model', required=True)
     parser.add_argument('--debug', help='test model', required=False, default='false')
     parser.add_argument('--tg', help='Use TG masks', required=False, default='false')
     parser.add_argument('--epochs', help='# of epochs', required=False, default=2)
     
     args = parser.parse_args()
-    runmodel = bool({'true': True, 'false': False}[args.runmodel.lower()])
     tg = bool({'true': True, 'false': False}[args.tg.lower()])
     debug = bool({'true': True, 'false': False}[args.debug.lower()])
     animal = args.animal
@@ -46,77 +44,80 @@ if __name__ == '__main__':
         indices = indices[0:10]
         dataset = torch.utils.data.Subset(dataset, indices)
 
+    workers = 1
+    if torch.cuda.is_available(): 
+        device = torch.device('cuda') 
+        batch_size = 32
+        print(f'Using Nvidia graphics card GPU with {workers} workers at a batch size of {batch_size}')
+    else:
+        device = torch.device('cpu')
+        batch_size = 1
+        print(f'Using CPU with {workers} workers at a batch size of {batch_size}')
 
     # define training and validation data loaders
     # multiprocessing with something other than 0 workers doesn't work on current
     # version of python's multiprocessing. Using 0 turns it off
-    workers = 1
     data_loader = torch.utils.data.DataLoader(
-                dataset, batch_size=2, shuffle=True, num_workers=workers,
+                dataset, batch_size=batch_size, shuffle=True, num_workers=workers,
                 collate_fn=utils.collate_fn)
     print(f"We have: {len(dataset)} images to train.")
 
-    if torch.cuda.is_available(): 
-        device = torch.device('cuda') 
-        print('Using Nvidia graphics card GPU')
-    else:
-        device = torch.device('cpu')
-        print('Using CPU')
     # our dataset has two classs, tissue or 'not tissue'
     num_classes = 2
     modelpath = os.path.join(ROOT, 'mask.model.pth')
-    if runmodel:
-        # create logging file
-        logpath = os.path.join(ROOT, "mask.logger.txt")
-        logfile = open(logpath, "w")
-        logheader = f"Masking {datetime.now()} with {epochs} epochs\n"
-        logfile.write(logheader)
-        # get the model using our helper function
-        model = get_model_instance_segmentation(num_classes)
-        # move model to the right device
-        model.to(device)
-        # construct an optimizer
-        params = [p for p in model.parameters() if p.requires_grad]
-        optimizer = torch.optim.SGD(params, lr=0.005,momentum=0.9, weight_decay=0.0005)
-        # and a learning rate scheduler which decreases the learning rate by # 10x every 3 epochs
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+    # create logging file
+    logpath = os.path.join(ROOT, "mask.logger.txt")
+    logfile = open(logpath, "w")
+    logheader = f"Masking {datetime.now()} with {epochs} epochs\n"
+    logfile.write(logheader)
+    # get the model using our helper function
+    model = get_model_instance_segmentation(num_classes)
+    # move model to the right device
+    model.to(device)
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler which decreases the learning rate by # 10x every 3 epochs
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
-        """
-        for epoch in range(epochs):
-            # train for one epoch, printing every 10 iterations
-            mlogger = train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
-            smlogger = str(mlogger) + "\n"
-            logfile.write(smlogger)
-            # update the learning rate
-            lr_scheduler.step()
-            if not debug:
-                torch.save(model.state_dict(), modelpath)
-        """
-
-        # Perform training loop for n epochs
-        loss_list = []
-        model.train()
-        for epoch in range(epochs):
-            loss_epoch = []
-            iteration=1
-            for images,targets in tqdm(data_loader):
-                images = list(image.to(device) for image in images)
-                targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-                optimizer.zero_grad()
-                model=model.double()
-                loss_dict = model(images, targets)
-                losses = sum(loss for loss in loss_dict.values())
-                losses.backward()       
-                optimizer.step()
-                loss_epoch.append(losses.item())
-                iteration+=1
-            loss_epoch_mean = np.mean(loss_epoch) 
-            loss_list.append(loss_epoch_mean) 
-            smlogger = str(loss_epoch_mean) + ",\n"
-
-            print("Average loss  = {:.4f} ".format(loss_epoch_mean))
-            logfile.write(smlogger)
+    """
+    for epoch in range(epochs):
+        # train for one epoch, printing every 10 iterations
+        mlogger = train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        smlogger = str(mlogger) + "\n"
+        logfile.write(smlogger)
+        # update the learning rate
+        lr_scheduler.step()
+        if not debug:
             torch.save(model.state_dict(), modelpath)
-        print('Finished with masks')
-        logfile.close()
+    """
+
+    # Perform training loop for n epochs
+    loss_list = []
+    model.train()
+    for epoch in range(epochs):
+        loss_epoch = []
+        iteration=1
+        for images,targets in tqdm(data_loader):
+            images = list(image.to(device) for image in images)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            optimizer.zero_grad()
+            model=model.double()
+            loss_dict = model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+            losses.backward()       
+            optimizer.step()
+            loss_epoch.append(losses.item())
+            iteration+=1
+
+        loss_epoch_mean = np.mean(loss_epoch) 
+        loss_list.append(loss_epoch_mean) 
+        print("Average loss  = {:.4f} ".format(loss_epoch_mean))
+        torch.save(model.state_dict(), modelpath) # save each epoch
+        
+    logfile.write(str(loss_list))
+    logfile.write("\n")
+
+    print('Finished with masks')
+    logfile.close()
 
