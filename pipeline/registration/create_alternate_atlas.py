320dd1578a6895fa630e8bed13d68be70cf15357
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-09-21 10:57:07 +0700

    Added argparse to create alt atlas fixed paths #1

--- pipeline/registration/create_alternate_atlas.py
+++ pipeline/registration/create_alternate_atlas.py
@@ -8,23 +8,22 @@ from scipy.ndimage import zoom
 
 def create_boundary_mesh(animal):
 
-    ROOT = '/net/birdstore/Active_Atlas_Data/data_root/pipeline_data'
+    ROOT = f'/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/{animal}/preps/CH1'
 
-    boundary_tiff_path = os.path.join(ROOT, animal, 'preps/CH1/allen_mouse_25um', 'boundaries.tiff')
-    #boundary_np_path = os.path.join(ROOT, animal, 'preps/CH1/brainreg_allen', 'boundary.npy')
+    boundary_tiff_path = os.path.join(ROOT, 'allen_mouse_25um', 'boundaries.tiff')
     volume = io.imread(boundary_tiff_path)
 
     print(f'volume info {volume.shape}, {volume.dtype}')
 
-    fpath = os.path.join(ROOT, animal,  'preps/CH1/thumbnail_aligned')
+    fpath = os.path.join(ROOT, 'thumbnail_aligned')
     files = os.listdir(fpath)
     z = len(files)
     midfile = str(z // 2).zfill(3) + ".tif"
-    midfilepath = os.path.join(ROOT, animal,  f'preps/CH1/thumbnail_aligned/{midfile}')
+    midfilepath = os.path.join(ROOT, f'thumbnail_aligned/{midfile}')
     mid_arr = io.imread(midfilepath)
     print(f'midfile into {midfile}, {mid_arr.shape}, {z}') 
 
-    boundary_outpath = os.path.join(ROOT, animal, 'preps/CH1/boundary')
+    boundary_outpath = os.path.join(ROOT, 'boundary')
     os.makedirs(boundary_outpath, exist_ok=True)
     arr = volume.copy()
     sagittal_arr = np.zeros((arr.shape[1], arr.shape[0], arr.shape[2]))
@@ -39,7 +38,7 @@ def create_boundary_mesh(animal):
     sagittal_arr = sagittal_arr.astype(np.uint8)
     print(f'sagittal info {sagittal_arr.shape}, {sagittal_arr.dtype}')
 
-    sagittal_np_path = os.path.join(ROOT, animal, 'preps/CH1/brainreg_allen', 'sagittal_boundary')
+    sagittal_np_path = os.path.join(ROOT, 'allen_mouse_25um', 'sagittal_boundary')
     np.save(sagittal_np_path, sagittal_arr)
 
     change_y = mid_arr.shape[0] / sagittal_arr.shape[0]
@@ -52,11 +51,11 @@ def create_boundary_mesh(animal):
     print('zoomed volume info',zoomed_sagittal_arr.shape)
     print('original volume info', mid_arr.shape, z)
 
-    zoomed_sagittal_np_path = os.path.join(ROOT, animal, 'preps/CH1/brainreg_allen', 'zoomed_sagittal_boundary')
+    zoomed_sagittal_np_path = os.path.join(ROOT, 'allen_mouse_25um', 'zoomed_sagittal_boundary')
     zoomed_sagittal_arr = zoomed_sagittal_arr.astype(np.uint8)
     np.save(zoomed_sagittal_np_path, zoomed_sagittal_arr)
 
-    zoomed_boundary_outpath = os.path.join(ROOT, animal, 'preps/CH1/zoomed_boundary')
+    zoomed_boundary_outpath = os.path.join(ROOT, 'allen_mouse_25um', 'zoomed_boundary')
     os.makedirs(zoomed_boundary_outpath, exist_ok=True)
     endsection = zoomed_sagittal_arr.shape[2]   
     for i in tqdm(range(0, endsection, 1)):
