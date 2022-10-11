f4235ddadd2a1963626f3b0d551556aa557012ce
drinehart1 <duane.rinehart@gmail.com>
2022-10-11 09:52:31 -0700

    removed Java environment variable reference

--- pipeline/lib/pipeline.py
+++ pipeline/lib/pipeline.py
@@ -125,9 +125,7 @@ class Pipeline(
         If the check failed, check the workernoshell.err.log in your project directory for more information
         """
         start_time = timer()
-        # os.environ["_JAVA_OPTIONS"] = "-Xmx10g"  # DEPRECATED - JUN-2022? (REMOVE)
-        # os.environ["export CV_IO_MAX_IMAGE_PIXELS"] = "21474836480"  # DEPRECATED - JUN-2022? (REMOVE)
-
+        
         error = ""
         if not os.path.exists("/usr/local/share/bftools/showinf"):
             error += "showinf in bftools is not installed"
