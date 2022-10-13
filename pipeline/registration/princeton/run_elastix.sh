06911e2cc05256ac6a48d7ec2e847bb71e57f621
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-10-13 09:34:39 -0400

    Added a script to run elastix and the parameter files

---
+++ pipeline/registration/princeton/run_elastix.sh
@@ -0,0 +1,5 @@
+elastix -f /home/eddyod/.brainglobe/sagittal_atlas_20um_iso.tif \
+-m /net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK37/preps/CH1/downsized_for_atlas.tif \
+-out /net/birdstore/Active_Atlas_Data/data_root/pipeline_data/DK37/preps/CH1/thumbnail_aligned/elastix \
+ -p /home/eddyod/programming/preprocessing-pipeline/pipeline/registration/princeton/parameterfolder/Order2_Par0000bspline.txt \
+ -p /home/eddyod/programming/preprocessing-pipeline/pipeline/registration/princeton/parameterfolder/Order1_Par0000affine.txt
