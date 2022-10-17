5a061df3e829fb4293f46f5540e590724c6e0119
madwilliam5225 <madwilliam5225@gmail.com>
2022-10-17 16:27:06 -0700

    updates

--- pipeline/cell_extractor/test/test.py
+++ pipeline/cell_extractor/test/test.py
@@ -1,6 +1,23 @@
-import sys
-import os
-sys.path.append(os.path.abspath('./preprocessing-pipeline/pipeline'))
+import matplotlib.pyplot as plt
+import numpy as np
+import os,sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)+'/../../'))
+from cell_extractor.diagnostics.ToolKit import get_DataFrame_from_query_result,find_equivalence,find_agreement,get_DataFrame_from_detection_df
+from Controllers.MarkedCellController import MarkedCellController
+from Controllers.SqlController import SqlController
+import pandas as pd
+from cell_extractor.CellAnnotationUtilities import CellAnnotationUtilities
+from cell_extractor.AnnotationProximityTool import AnnotationProximityTool
+from matplotlib.patches import Patch
+from collections import Counter
+from lib.UrlGenerator import UrlGenerator
 from cell_extractor.QcGenerater import QcGenerater
-generater = QcGenerater('DK41',3)
-generater.get_sure_and_unsure_cells([500,500])
+
+animal = 'DK41'
+controller = SqlController()
+factor = controller.get_resolution(animal)
+
+controller = MarkedCellController()
+search_dict = {'FK_prep_id':animal,'FK_cell_type_id':26}
+mixed = get_DataFrame_from_query_result(controller.get_marked_cells(search_dict),'Sure',factor)
+mixed = find_equivalence(mixed)
