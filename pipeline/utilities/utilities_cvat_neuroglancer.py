1e27b88eb7dcec03e382efd8b4fe33835ea43df6
drinehart1 <duane.rinehart@gmail.com>
2022-11-14 15:43:49 -0800

    sphinx doc generation for pipeline

--- pipeline/utilities/utilities_cvat_neuroglancer.py
+++ pipeline/utilities/utilities_cvat_neuroglancer.py
@@ -14,16 +14,25 @@ from matplotlib import colors
 from pylab import cm
 from collections import defaultdict
 from controller.sql_controller import SqlController
-
 from utilities.utilities_process import get_cpus
 
-def calculate_chunks(downsample, mip):
-    """
-    Chunks default to 64,64,64 so we want different chunks at 
-    different resolutions
 
-    #highest resolution tier (mip)  is 0 and increments
-    """
+def calculate_chunks(downsample, mip):
+    '''Function returns chunk sizes for different 'precomputed cloud volume' image stack resolutions
+
+    Image stack will be created from full-resolution images but must be chunked for efficient storage and loading into browser.
+    Default values are [64, 64, 64] but may be modified for different resolutions.
+    More info: https://github.com/seung-lab/cloud-volume
+
+    Note: highest resolution tier (mip)  is 0 and increments
+
+    :param downsample:
+    :type downsample:
+    :param mip:
+    :type mip:
+    :return:
+    :rtype:
+    '''
     d = defaultdict(dict)
     result = [64, 64, 64]
     d[False][-1] = [1024, 1024, 1]
@@ -50,10 +59,17 @@ def calculate_chunks(downsample, mip):
         result = [64,64,64]
     return result
 
+
 def calculate_factors(downsample, mip):
-    """
-    Scales get calculated by default by 2x2x1 downsampling
-    """
+    '''Scales get calculated by default by 2x2x1 downsampling
+
+    :param downsample:
+    :type downsample:
+    :param mip:
+    :type mip:
+    :return:
+    :rtype:
+    '''
     d = defaultdict(dict)
     result = [2,2,1]
     d[False][0] = result
@@ -77,7 +93,14 @@ def calculate_factors(downsample, mip):
         result = [2,2,1]
     return result
 
+
 def get_db_structure_infos():
+    '''Unknown - Needs more info [possibly just for testing and may be removed]
+    Comment based on hard-coded 'MD589'
+
+    :return:
+    :rtype:
+    '''
     sqlController = SqlController('MD589')
     db_structures = sqlController.get_structures_dict()
     structures = {}
@@ -87,7 +110,15 @@ def get_db_structure_infos():
         structures[structure] = v
     return structures
 
-def get_known_foundation_structure_names():
+
+def get_known_foundation_structure_names() -> list[str]:
+    '''Unknown - Needs more info [possibly just for testing and may be removed]
+    Comment based on presence of these same structure names in database (not static) but, if required,
+    should be moved to static variable declaration (lookup tables)
+
+    :return:
+    :rtype:
+    '''
     known_foundation_structures = ['MVePC', 'DTgP', 'VTA', 'Li', 'Op', 'Sp5C', 'RPC', 'MVeMC', 'APT', 'IPR',
                                    'Cb', 'pc', 'Amb', 'SolIM', 'Pr5VL', 'IPC', '8n', 'MPB', 'Pr5', 'SNR',
                                    'DRD', 'PBG', '10N', 'VTg', 'R', 'IF', 'RR', 'LDTg', '5TT', 'Bar',
@@ -98,7 +129,16 @@ def get_known_foundation_structure_names():
                                    'VCP', 'CnF', 'Sol', 'IPL', 'X', 'AP', 'MiTg', 'DRI', 'RPF', 'VLL']
     return known_foundation_structures
 
-def get_structure_number(structure):
+
+def get_structure_number(structure: str) -> str:
+    '''Unknown - Needs more info [possibly just for testing and may be removed]
+    Likely deprecated as we do not use 'color' attribute of brains and call to get_db_structure_infos(), which has hard-coded 'MD589'
+
+    :param structure:
+    :type structure: str
+    :return:
+    :rtype: str
+    '''
     db_structure_infos = get_db_structure_infos()
     known_foundation_structure_names = get_known_foundation_structure_names()
     non_db_structure_names = [structure for structure in known_foundation_structure_names if structure not in db_structure_infos.keys()]
@@ -111,7 +151,16 @@ def get_structure_number(structure):
         color = 255
     return color
 
-def get_segment_properties(all_known=False):
+
+def get_segment_properties(all_known: bool = False):
+    '''Unknown - Needs more info [possibly just for testing and may be removed]
+    Comment based on call to get_db_structure_infos(), which has hard-coded 'MD589'
+
+    :param all_known:
+    :type all_known:
+    :return:
+    :rtype:
+    '''
     db_structure_infos = get_db_structure_infos()
     known_foundation_structure_names = get_known_foundation_structure_names()
     non_db_structure_names = [structure for structure in known_foundation_structure_names if structure not in db_structure_infos.keys()]
@@ -122,21 +171,69 @@ def get_segment_properties(all_known=False):
 
     return segment_properties
 
+
 def get_segment_ids(volume):
+    '''Unknown - Needs more info
+
+    :param volume:
+    :type volume:
+    :return:
+    :rtype:
+    '''
     ids = [int(i) for i in np.unique(volume[:])]
     segment_properties = [(number, f'{number}: {number}') for number in ids]
     return segment_properties
 
-def get_hex_from_id(id, colormap='coolwarm'):
+
+def get_hex_from_id(id, colormap: str = 'coolwarm'):
+    '''Unknown - Needs more info [possibly just for testing and may be removed]
+    Likely deprecated as we do not use 'color' attribute of brains
+
+    :param id:
+    :type id:
+    :param colormap: str
+    :type colormap:
+    :return:
+    :rtype:
+    '''
     cmap = cm.get_cmap(colormap, 255)
     rgba = cmap(id)
     return colors.rgb2hex(rgba)
 
+
 class NumpyToNeuroglancer():
+    '''Contains collection of methods used to transform Numpy arrays into 'precomputed cloud volume' format
+    More info: https://github.com/seung-lab/cloud-volume
+
+    Methods
+    -------
+    __init__()
+    init_precomputed()
+    init_volume()
+    add_segment_properties()
+    add_rechunking()
+    add_downsampled_volumes()
+    add_segmentation_mesh()
+    process_simple_slice()
+    process_mesh()
+    process_coronal_slice()
+    process_image()
+    get_dimensions()
+    add_annotation_layer()
+    add_annotation_point_set()
+    add_annotation_point()
+    clear_layer()
+    add_layer()
+    init_layer()
+    report_new_layer()
+    add_volume()
+    preview()
+
+    '''
     viewer = None
 
-    def __init__(self, animal, volume, scales, layer_type, data_type, num_channels=1, 
-        chunk_size=[256,256,128], offset=[0,0,0]):
+    def __init__(self, animal: str, volume, scales, layer_type, data_type, num_channels=1,
+        chunk_size: list[int] = [256,256,128], offset: list[int] = [0,0,0]):
         self.volume = volume
         self.scales = scales
         self.layer_type = layer_type
@@ -148,7 +245,21 @@ class NumpyToNeuroglancer():
         self.animal = animal
         self.num_channels = num_channels
 
-    def init_precomputed(self, path, volume_size, starting_points=None, progress_id=None):
+
+    def init_precomputed(self, path: str, volume_size, starting_points=None, progress_id=None) -> None:
+        '''Initializes 'precomputed' cloud volume format (directory holding multiple volumes)
+
+        :param path:
+        :type path: str
+        :param volume_size:
+        :type volume_size:
+        :param starting_points:
+        :type starting_points:
+        :param progress_id:
+        :type progress_id:
+        :return:
+        :rtype:
+        '''
         info = CloudVolume.create_new_info(
             num_channels=self.num_channels,
             layer_type=self.layer_type,  # 'image' or 'segmentation'
@@ -165,7 +276,15 @@ class NumpyToNeuroglancer():
         self.precomputed_vol.commit_info()
         self.precomputed_vol.commit_provenance()
 
-    def init_volume(self, path):
+
+    def init_volume(self, path: str) -> None:
+        '''Initializes 'precomputed' cloud volume ('volume' is a collection image stack with same resolution)
+
+        :param path:
+        :type path: str
+        :return:
+        :rtype:
+        '''
         info = CloudVolume.create_new_info(
             num_channels = self.volume.shape[2] if len(self.volume.shape) > 2 else 1,
             layer_type = self.layer_type,
@@ -181,7 +300,15 @@ class NumpyToNeuroglancer():
         self.precomputed_vol.commit_info()
         self.precomputed_vol.commit_provenance()
 
-    def add_segment_properties(self, segment_properties):
+
+    def add_segment_properties(self, segment_properties) -> None:
+        '''Augments 'precomputed' cloud volume with attribute tags [resolution, chunk size]
+
+        :param segment_properties:
+        :type segment_properties:
+        :return:
+        :rtype:
+        '''
         if self.precomputed_vol is None:
             raise NotImplementedError('You have to call init_precomputed before calling this function.')
 
@@ -205,7 +332,20 @@ class NumpyToNeuroglancer():
         with open(os.path.join(segment_properties_path, 'info'), 'w') as file:
             json.dump(info, file, indent=2)
 
-    def add_rechunking(self, outpath, downsample, chunks=None):
+
+    def add_rechunking(self, outpath: str, downsample, chunks=None) -> None:
+        '''Augments 'precomputed' cloud volume with additional chunk calculations
+        [so format has pointers to location of individual volumes?]
+
+        :param outpath:
+        :type outpath: str
+        :param downsample:
+        :type downsample:
+        :param chunks:
+        :type chunks:
+        :return:
+        :rtype:
+        '''
         if self.precomputed_vol is None:
             raise NotImplementedError('You have to call init_precomputed before calling this function.')
         cpus, _ = get_cpus()
@@ -218,7 +358,17 @@ class NumpyToNeuroglancer():
         tq.insert(tasks)
         tq.execute()
 
-    def add_downsampled_volumes(self, chunk_size=[128, 128, 64], num_mips=4):
+
+    def add_downsampled_volumes(self, chunk_size: list[int] = [128, 128, 64], num_mips: int = 4) -> None:
+        '''Augments 'precomputed' cloud volume with additional resolutions using chunk calculations
+
+        :param chunk_size:
+        :type chunk_size: list[int]
+        :param num_mips:
+        :type num_mips: int
+        :return:
+        :rtype:
+        '''
         if self.precomputed_vol is None:
             raise NotImplementedError('You have to call init_precomputed before calling this function.')
         _, cpus = get_cpus()
@@ -228,7 +378,17 @@ class NumpyToNeuroglancer():
         tq.insert(tasks)
         tq.execute()
 
-    def add_segmentation_mesh(self, shape=[448, 448, 448], mip=0):
+
+    def add_segmentation_mesh(self, shape: list[int] = [448, 448, 448], mip: int = 0) -> None:
+        '''Augments 'precomputed' cloud volume with segmentation mesh
+
+        :param shape:
+        :type shape: list[int]
+        :param mip:
+        :type mip: int
+        :return:
+        :rtype:
+        '''
         if self.precomputed_vol is None:
             raise NotImplementedError('You have to call init_precomputed before calling this function.')
 
@@ -239,12 +399,21 @@ class NumpyToNeuroglancer():
                                         shape=shape, compress=True) # The first phase of creating mesh
         tq.insert(tasks)
         tq.execute()
-        # It should be able to incoporated to above tasks, but it will give a weird bug. Don't know the reason
+        # It should be able to incorporate to above tasks, but it will give a weird bug. Don't know the reason
         tasks = tc.create_mesh_manifest_tasks(self.precomputed_vol.layer_cloudpath) # The second phase of creating mesh
         tq.insert(tasks)
         tq.execute()
 
+
     def process_simple_slice(self, file_key):
+        '''Unknown role but method a)loops through image stack, b) loads into Numpy array, c) reshapes in single dimension,
+        d) adds to 'precomputed' cloud volume, e) touches files to track progress
+
+        :param file_key:
+        :type file_key:
+        :return:
+        :rtype:
+        '''
         index, infile = file_key
         print(index, infile)
         try:
@@ -260,7 +429,18 @@ class NumpyToNeuroglancer():
         image.close()
         return
 
+
     def process_mesh(self, file_key):
+        '''Unknown role but method a) loops through image stack, b) loads into Numpy array, c) applies mask?
+        d) transposes Numpy array, e) adds to 'precomputed' cloud volume
+
+        Unclear where values for labels range came from
+
+        :param file_key:
+        :type file_key:
+        :return:
+        :rtype:
+        '''
         index, infile = file_key
         if os.path.exists(os.path.join(self.progress_dir, os.path.basename(infile))):
             print(f"Section {index} already processed, skipping ")
@@ -280,7 +460,18 @@ class NumpyToNeuroglancer():
         del img
         return
 
+
     def process_coronal_slice(self, file_key):
+        '''Unknown role but method a) loops through image stack, b) loads into Numpy array, c) reshapes each image
+        d) adds to 'precomputed' cloud volume
+
+        Only for coronal-sliced brains?
+
+        :param file_key:
+        :type file_key:
+        :return:
+        :rtype:
+        '''
         index, infile = file_key
         if os.path.exists(os.path.join(self.progress_dir, os.path.basename(infile))):
             print(f"Slice {index} already processed, skipping ")
@@ -299,7 +490,15 @@ class NumpyToNeuroglancer():
         del img
         return
 
+
     def process_image(self, file_key):
+        '''Unknown role but method a) reads single file, b) reshapes/transforms, c) adds to 'precomputed' cloud volume
+
+        :param file_key:
+        :type file_key:
+        :return:
+        :rtype:
+        '''
         index, infile, orientation, progress_dir = file_key
         basefile = os.path.basename(infile)
         progress_file = os.path.join(progress_dir, basefile)
@@ -327,17 +526,51 @@ class NumpyToNeuroglancer():
         del img
         return
 
+
     def get_dimensions(self):
+        '''Unknown - Needs more info
+
+        :return:
+        :rtype:
+        '''
         return neuroglancer.CoordinateSpace(names=['x', 'y', 'z'], units='nm', scales=self.scales)
 
-    def add_annotation_layer(self,layer_name,annotations):
+
+    def add_annotation_layer(self, layer_name, annotations) -> None:
+        '''Unknown - Needs more info
+
+        adds annotation layer? If so, not part of preprocessing-pipeline
+
+        :param layer_name:
+        :type layer_name:
+        :param annotations:
+        :type annotations:
+        :return:
+        :rtype:
+        '''
         dimensions = self.get_dimensions()
         with self.viewer.txn() as s:
             s.layers.append(name=layer_name,
                     layer=neuroglancer.LocalAnnotationLayer(dimensions=dimensions,
                     annotations=annotations))
 
-    def add_annotation_point_set(self,cooridnate_list,point_id_list = None,layer_name=None, clear_layer=False):
+
+    def add_annotation_point_set(self, cooridnate_list, point_id_list = None, layer_name=None, clear_layer: bool = False) -> None:
+        '''Unknown - Needs more info
+
+        adds coordinate points to annotation layer to Neuroglancer? If so, not part of preprocessing-pipeline
+
+        :param cooridnate_list:
+        :type cooridnate_list:
+        :param point_id_list:
+        :type point_id_list:
+        :param layer_name:
+        :type layer_name:
+        :param clear_layer:
+        :type clear_layer:
+        :return:
+        :rtype:
+        '''
         n_annotations = len(cooridnate_list)
         if point_id_list == None:
             point_id_list = [str(elementi) for elementi in list(range(n_annotations))]
@@ -349,31 +582,100 @@ class NumpyToNeuroglancer():
         self.add_annotation_layer(layer_name,annotations)
         self.report_new_layer(layer_name)
 
-    def add_annotation_point(self,cooridnate,point_id='pointi',layer_name=None, clear_layer=False):
+
+    def add_annotation_point(self, cooridnate, point_id='pointi', layer_name=None, clear_layer: bool = False) -> None:
+        '''Unknown - Needs more info
+
+        adds single coordinate point to annotation layer to Neuroglancer? If so, not part of preprocessing-pipeline
+
+        :param cooridnate:
+        :type cooridnate:
+        :param point_id:
+        :type point_id:
+        :param layer_name:
+        :type layer_name:
+        :param clear_layer:
+        :type clear_layer:
+        :return:
+        :rtype:
+        '''
         self.init_layer(clear_layer)
         annotations=[neuroglancer.PointAnnotation(id=point_id,point=cooridnate)]
         self.add_annotation_layer(layer_name,annotations)
         self.report_new_layer(layer_name)
 
-    def clear_layer(self,clear_layer = False):
+
+    def clear_layer(self, clear_layer: bool = False) -> None:
+        '''Unknown - Needs more info
+
+        removes annotation layer from Neuroglancer? If so, not part of preprocessing-pipeline
+
+        :param clear_layer:
+        :type clear_layer: bool
+        :return:
+        :rtype:
+        '''
         if clear_layer:
             with self.viewer.txn() as s:
                     s.layers.clear()
 
-    def add_layer(self,layer_name,layer):
+
+    def add_layer(self, layer_name: str, layer) -> None:
+        '''Unknown - Needs more info
+
+        adds annotation layer to Neuroglancer? If so, not part of preprocessing-pipeline
+
+        :param layer_name:
+        :type layer_name: str
+        :param layer:
+        :type layer:
+        :return:
+        :rtype:
+        '''
         with self.viewer.txn() as s:
             s.layers[layer_name] = layer
     
-    def init_layer(self,clear_layer):
+    def init_layer(self, clear_layer) -> None:
+        '''Unknown - Needs more info
+
+        initializes [image or annotation] layer for Neuroglancer? If so, not part of preprocessing-pipeline
+
+        :param clear_layer:
+        :type clear_layer:
+        :return:
+        :rtype:
+        '''
         if self.viewer is None:
             self.viewer = neuroglancer.Viewer()
         self.clear_layer(clear_layer)
 
-    def report_new_layer(self,layer_name):
+
+    def report_new_layer(self, layer_name: str) -> None:
+        '''Appears to be for debugging; REMOVE
+
+        :param layer_name:
+        :type layer_name:
+        :return:
+        :rtype:
+        '''
         print(f'A new layer named {layer_name} is added to:')
         print(self.viewer)
 
-    def add_volume(self,volume,layer_name=None, clear_layer=False):
+
+    def add_volume(self, volume, layer_name: str = None, clear_layer: bool = False) -> None:
+        '''Unknown - Needs more info
+
+        adds volume to precomputed cloud volume format?
+
+        :param volume:
+        :type volume:
+        :param layer_name:
+        :type layer_name:
+        :param clear_layer:
+        :type clear_layer:
+        :return:
+        :rtype:
+        '''
         self.init_layer(clear_layer)
         if layer_name is None:
             layer_name = f'{self.layer_type}_{self.scales}'
@@ -387,12 +689,31 @@ class NumpyToNeuroglancer():
             layer = neuroglancer.ImageLayer(source=source)
         self.add_layer(layer_name,layer)
         self.report_new_layer(layer_name)
-        
 
-    def preview(self, layer_name=None, clear_layer=False):
+
+    def preview(self, layer_name: str = None, clear_layer: bool = False) -> None:
+        '''Unknown - Needs more info
+
+        adds volume to precomputed cloud volume format? Name inconsistent with function
+
+        :param layer_name:
+        :type layer_name:
+        :param clear_layer:
+        :type clear_layer:
+        :return:
+        :rtype:
+        '''
         self.add_volume(self.volume,layer_name=layer_name, clear_layer=clear_layer)
 
+
 def mask_to_shell(mask):
+    '''Unknown - Needs more info
+
+    :param mask:
+    :type mask:
+    :return:
+    :rtype:
+    '''
     sub_contours = measure.find_contours(mask, 1)
     sub_shells = []
     for sub_contour in sub_contours:
@@ -404,4 +725,3 @@ def mask_to_shell(mask):
     shell = np.array(sub_shells).sum(axis=0)
     del sub_shells
     return shell
-
