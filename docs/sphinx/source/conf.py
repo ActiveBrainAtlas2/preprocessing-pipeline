416bb81fbe8c28d40a997e63602b280dddda1ccc
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-11-19 07:19:51 -0500

    Added more mock imports to sphinx conf: igneous

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -28,7 +28,7 @@ extensions = [
     'sphinx.ext.autosummary',  # Create neat summary tables
 ]
 autosummary_generate = False  # Turn on sphinx.ext.autosummary
-autodoc_mock_imports = ["settings", "SimpleITK-SimpleElastix", "cloudvolume", "neuroglancer", "taskqueue", "torch"]
+autodoc_mock_imports = ["settings", "SimpleITK-SimpleElastix", "cloudvolume", "neuroglancer", "taskqueue", "torch", "igneous"]
 
 
 
