ab3ac5329018889bc78a2c6def26dee10dc6dfbb
Edward O'Donnell <eddy.odonnell@gmail.com>
2023-05-17 13:24:41 +0700

    Removed cropping from create volume

--- src/registration/scripts/stack2volume2atlas.py
+++ src/registration/scripts/stack2volume2atlas.py
@@ -320,7 +320,7 @@ class VolumeRegistration:
         for ffile in tqdm(files):
             fpath = os.path.join(self.thumbnail_aligned, ffile)
             farr = read_image(fpath)
-            farr = farr[200:-200,200:-200]
+            #farr = farr[200:-200,200:-200]
             file_list.append(farr)
         image_stack = np.stack(file_list, axis = 0)
         io.imsave(self.moving_volume_path, image_stack.astype(np.uint16))
