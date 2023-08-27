acb600cb258a0991bea05401e052c9421aec0817
Edward O'Donnell <eddy.odonnell@gmail.com>
2023-08-27 12:53:05 +0700

    Set lod=1 and mag=3

--- src/pipeline/scripts/create_neuroglancer_mesh.py
+++ src/pipeline/scripts/create_neuroglancer_mesh.py
@@ -33,7 +33,7 @@ from library.image_manipulation.filelocation_manager import FileLocationManager
 from library.image_manipulation.neuroglancer_manager import NumpyToNeuroglancer, MESHDTYPE
 from library.utilities.utilities_process import get_cpus, get_hostname
 
-def create_mesh(animal, limit, scaling_factor, skeleton, debug):
+def create_mesh(animal, limit, scaling_factor, skeleton, multires=True, debug=False):
     sqlController = SqlController(animal)
     fileLocationManager = FileLocationManager(animal)
     xy = sqlController.scan_run.resolution * 1000
@@ -152,13 +152,14 @@ def create_mesh(animal, limit, scaling_factor, skeleton, debug):
     # lod=2: 176M 0.shard
 
     # 
-    lods = 1
-    print(f'Creating sharded multires task with {cpus} CPUs with LODs={lods}')
-    tasks = tc.create_sharded_multires_mesh_tasks(layer_path, num_lod=lods)
-    tq.insert(tasks)    
-    tq.execute()
+    if multires:
+        lods = 1
+        print(f'Creating sharded multires task with {cpus} CPUs with LODs={lods}')
+        tasks = tc.create_sharded_multires_mesh_tasks(layer_path, num_lod=lods)
+        tq.insert(tasks)    
+        tq.execute()
     
-    magnitude = 5
+    magnitude = 3
     print(f'Creating meshing manifest tasks with {cpus} CPUs with magnitude={magnitude}')
     tasks = tc.create_mesh_manifest_tasks(layer_path, magnitude=magnitude) # The second phase of creating mesh
     tq.insert(tasks)
@@ -184,13 +185,15 @@ if __name__ == '__main__':
     parser.add_argument('--limit', help='Enter the # of files to test', required=False, default=0)
     parser.add_argument('--scaling_factor', help='Enter an integer that will be the denominator', required=False, default=1)
     parser.add_argument("--skeleton", help="Create skeletons", required=False, default=False)
+    parser.add_argument("--multires", help="Create multiple resolutions", required=False, default=False)
     parser.add_argument("--debug", help="debug", required=False, default=False)
     args = parser.parse_args()
     animal = args.animal
     limit = int(args.limit)
     scaling_factor = int(args.scaling_factor)
     skeleton = bool({"true": True, "false": False}[str(args.skeleton).lower()])
+    multires = bool({"true": True, "false": False}[str(args.multires).lower()])
     debug = bool({"true": True, "false": False}[str(args.debug).lower()])
     
-    create_mesh(animal, limit, scaling_factor, skeleton, debug)
+    create_mesh(animal, limit, scaling_factor, skeleton, multires, debug)
 
