daf0b7d15ffbab023f0f76d123dbcb40fd73ae6a
Edward O'Donnell <eddy.odonnell@gmail.com>
2023-09-06 08:05:08 +0700

    Fixed default values for args

--- src/pipeline/scripts/create_pipeline.py
+++ src/pipeline/scripts/create_pipeline.py
@@ -71,7 +71,8 @@ if __name__ == "__main__":
     parser.add_argument("--downsample", help="Enter true or false", required=False, default="true")
     parser.add_argument("--debug", help="Enter true or false", required=False, default="false")
     parser.add_argument("--iterations", help="Enter iterations for alignment", required=False, default=1, type=int)
-    parser.add_argument("--tg", help="Extend the mask to expose the entire underside of the brain", required=False, default=False)
+    parser.add_argument("--tg", help="Extend the mask to expose the entire underside of the brain", 
+                        required=False, default=False)
     parser.add_argument("--task", 
                         help="Enter the task you want to perform: \
                         extract|mask|clean|histogram|align|create_metrics|extra_channel|neuroglancer|check_status",
@@ -88,7 +89,8 @@ if __name__ == "__main__":
     tg = bool({"true": True, "false": False}[str(args.tg).lower()])
     task = str(args.task).strip().lower()
 
-    pipeline = Pipeline(animal, rescan_number=0, channel=1, iterations=iterations, downsample=True, tg=False, task='status', debug=False)
+    pipeline = Pipeline(animal, rescan_number=rescan_number, channel=channel, iterations=iterations, 
+                        downsample=downsample, tg=tg, task=task, debug=False)
 
     function_mapping = {'extract': pipeline.extract,
                         'mask': pipeline.mask,
